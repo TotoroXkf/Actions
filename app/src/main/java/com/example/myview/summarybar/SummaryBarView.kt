acbab77634458d9d385b5fabd587682ee34c17d8
TotoroXkf <1131225133@qq.com>
2018-11-12 13:24:18 +0800

    Coding

--- app/src/main/java/com/example/myview/summarybar/SummaryBarView.kt
+++ app/src/main/java/com/example/myview/summarybar/SummaryBarView.kt
@@ -1,19 +1,19 @@
 package com.example.myview.summarybar
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.TypedValue
+import android.view.MotionEvent
 import com.example.myview.MyView
 
 class SummaryBarView : MyView {
+    //文本绘制画笔
     private lateinit var textPaint: Paint
+    //柱状图绘制画笔
     private lateinit var barPaint: Paint
-
-    //Bar列表
-    private val barList = ArrayList<Bar>()
+    //Bar的矩形框列表
+    private val barRectList = ArrayList<RectF>()
     //柱状图高度列表
     private val dataList = ArrayList<Float>()
     //底下文字列表
@@ -28,7 +28,10 @@ class SummaryBarView : MyView {
     var space = 0.0f
     //每个Bar的宽度
     var barWidth = 0.0f
-
+    //bar的数值映射为bar的高度的比例
+    var ratio = 0
+    //bar的最大高度
+    var max = 0f
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
@@ -57,12 +60,6 @@ class SummaryBarView : MyView {
 
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-
-        //计算最左边的
-    }
-
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
         if (animationEnable) {
@@ -72,23 +69,74 @@ class SummaryBarView : MyView {
         }
     }
 
+    /*
+     * 带动画绘制
+     */
     private fun drawWithoutAnimation(canvas: Canvas) {
-
+        //todo
     }
 
+    /*
+     * 不带动画绘制
+     */
     private fun drawWithAnimation(canvas: Canvas) {
-
+        //todo
     }
 
-    private inner class Bar {
-        //绘制柱状条的四个位置
-        var left: Int = 0
-        var top: Int = 0
-        var right: Int = 0
-        var bottom: Int = 0
+    /*
+     * 在末尾添加一个bar
+     */
+    fun addBar(data: Float) {
+        //todo
+    }
 
-        fun isInside(x: Float, y: Float): Boolean {
-            return x > left && x < right && y > top && y < bottom
+    fun addBars(list: List<Float>) {
+        //todo now
+        if (list.isEmpty()) {
+            return
+        }
+//        post {
+//            if (dataList.isEmpty()) {
+//                val start = paddingLeft + barWidth / 2
+//                xCoordinate.add(start)
+//                max = Math.max(max, list.max()!!)
+//                if (max > viewHeight) {
+//                    yCoordinate.add(viewHeight.toFloat())
+//                } else {
+//                }
+//                //val rect = Rect(start, list[0] * ratio, start + barWidth, list[0] * ratio)
+//            }
+//            for () {
+//
+//            }
+//            invalidate()
+//        }
+    }
+
+    /*
+     * 插入一个bar
+     */
+    fun insertBar(data: Float) {
+        //todo
+    }
+
+    /*
+     * 删除一个bar
+     */
+    fun removeBar(index: Int) {
+        //todo
+    }
+
+    /*
+     * 清空所有的bar
+     */
+    fun removeAll() {
+        //todo
+    }
+
+    private fun coordinateAdajust() {
+        post {
+
         }
     }
 }
