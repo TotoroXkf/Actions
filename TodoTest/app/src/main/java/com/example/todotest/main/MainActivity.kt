614dc9ee26b2205c351016eddbae604d07c4377e
TotoroXkf <1131225133@qq.com>
2019-01-01 22:18:13 +0800

    Coding

--- TodoTest/app/src/main/java/com/example/todotest/main/MainActivity.kt
+++ TodoTest/app/src/main/java/com/example/todotest/main/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.example.todotest.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.example.todotest.R
 import com.example.todotest.base.BaseActivity
+import kotlinx.android.synthetic.main.base_toolbar.*
 
 class MainActivity : BaseActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		
+		setSupportActionBar(toolbar)
+		supportActionBar?.setDisplayHomeAsUpEnabled(false)
 	}
 	
 	override fun getFragment(): Fragment {
 	
 	}
 	
-	override fun getLayoutId(): Int {
-	
-	}
+	override fun getLayoutId(): Int = R.layout.activity_main
 	
 }
