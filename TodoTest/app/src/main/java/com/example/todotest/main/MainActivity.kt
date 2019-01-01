ee6729bc83d2809eeb3dec8748932613091b5fde
TotoroXkf <1131225133@qq.com>
2019-01-01 13:44:03 +0800

    Coding

---
+++ TodoTest/app/src/main/java/com/example/todotest/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.todotest.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.example.todotest.R
+import com.example.todotest.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		
+	}
+	
+	override fun getFragment(): Fragment {
+	
+	}
+	
+	override fun getLayoutId(): Int {
+	
+	}
+	
+}
