07e9c3847dfa8b5cdb848a5cb8593389c8736bbd
TotoroXkf <1131225133@qq.com>
2019-01-06 10:45:41 +0800

    Coding

--- MyClock/app/src/main/java/com/example/myclock/ClockView.kt
+++ MyClock/app/src/main/java/com/example/myclock/ClockView.kt
@@ -2,12 +2,20 @@ package com.example.myclock
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class ClockView : View {
     private var viewWidth = 0
-    private var viewHight = 0
+    private var viewHeight = 0
+    private var paint = Paint()
+
+    private val HMS = 0
+    private val HM = 1
+    private var currentType = 1
+
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
@@ -17,17 +25,38 @@ class ClockView : View {
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     private fun init() {
-
+        paint.color = Color.WHITE
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         viewWidth = w
-        viewHight = h
+        viewHeight = h
+    }
+
+    fun setTypre(type: Int) {
+        currentType = type
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        if (canvas == null) {
+            return
+        }
+        if (currentType == HMS) {
+            drawHMS(canvas)
+        } else if (currentType == HM) {
+            drawHM(canvas)
+        }
+    }
+
+    private fun drawHM(canvas: Canvas) {
+
+    }
+
+    private fun drawHMS(canvas: Canvas) {
+
     }
 
 }
