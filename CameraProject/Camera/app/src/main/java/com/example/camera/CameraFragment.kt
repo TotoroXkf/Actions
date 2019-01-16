2ecfa3952f137c71583026db251b98b91fd568a2
TotoroXkf <1131225133@qq.com>
2019-01-16 18:09:56 +0800

    Coding

--- CameraProject/Camera/app/src/main/java/com/example/camera/CameraFragment.kt
+++ CameraProject/Camera/app/src/main/java/com/example/camera/CameraFragment.kt
@@ -1,16 +1,20 @@
 package com.example.camera
 
+import android.Manifest
 import android.content.Context
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.hardware.camera2.*
 import android.media.ImageReader
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class CameraFragment : Fragment() {
 	private val STATE_PREVIEW = 0
@@ -36,12 +40,12 @@ class CameraFragment : Fragment() {
 	): View? {
 		val view = inflater.inflate(R.layout.fragment_camera, container, false)
 		mSurfaceView = view.findViewById(R.id.surfaceView)
+		init()
 		return view
 	}
 	
 	override fun onActivityCreated(savedInstanceState: Bundle?) {
 		super.onActivityCreated(savedInstanceState)
-		init()
 	}
 	
 	private fun init() {
@@ -66,9 +70,111 @@ class CameraFragment : Fragment() {
 		})
 	}
 	
-	private fun initCameraAndPreview() {
+	private val mOnImageAvailableListener = { reader: ImageReader ->
+		//处理拍摄照片
 		
 	}
 	
+	private fun initCameraAndPreview() {
+		mHandlerThread = HandlerThread("Camera2")
+		mHandlerThread.start()
+		mHandler = Handler(mHandlerThread.looper)
+		try {
+			val cameraId = "" + CameraCharacteristics.LENS_FACING_FRONT
+			mCharacteristics = mCameraManager.getCameraCharacteristics(cameraId)
+			
+			mImageReader = ImageReader.newInstance(
+				mSurfaceView.width,
+				mSurfaceView.height,
+				ImageFormat.JPEG,
+				3
+			)
+			mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mHandler)
+			
+			val deviceStateCallback = object : CameraDevice.StateCallback() {
+				override fun onOpened(camera: CameraDevice) {
+					mCameraDevice = camera
+					Log.e("xkf123456789", "createCameraCaptureSession()")
+					createCameraCaptureSession()
+				}
+				
+				override fun onDisconnected(camera: CameraDevice) {
+				}
+				
+				override fun onError(camera: CameraDevice, error: Int) {
+				}
+				
+			}
+			
+			if (ActivityCompat.checkSelfPermission(
+					activity!!,
+					Manifest.permission.CAMERA
+				) != PackageManager.PERMISSION_GRANTED
+			) {
+				return
+			}
+			
+			mCameraManager.openCamera(cameraId, deviceStateCallback, mHandler)
+		} catch (e: CameraAccessException) {
+			Log.e("xkf123456789", "${e.message}")
+		}
+	}
 	
+	private fun createCameraCaptureSession() {
+		val sessionPreviewStateCallback = object : CameraCaptureSession.StateCallback() {
+			override fun onConfigureFailed(session: CameraCaptureSession) {
+			
+			}
+			
+			override fun onConfigured(session: CameraCaptureSession) {
+				mSession = session
+				setLocalParameter()
+				showPreView(getParameterMap())
+			}
+			
+		}
+		
+		mCameraDevice.createCaptureSession(
+			Arrays.asList(mSurfaceHolder.surface, mImageReader.surface),
+			sessionPreviewStateCallback, mHandler
+		)
+	}
+	
+	private val mSessionCaptureCallback = object : CameraCaptureSession.CaptureCallback() {
+		override fun onCaptureCompleted(
+			session: CameraCaptureSession,
+			request: CaptureRequest,
+			result: TotalCaptureResult
+		) {
+		
+		}
+	}
+	
+	private fun showPreView(parameterMap: HashMap<CaptureRequest.Key<Int>, Int>) {
+		mState = STATE_PREVIEW
+		try {
+			val builder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+			builder.addTarget(mSurfaceHolder.surface)
+			for ((key, value) in parameterMap) {
+				builder.set(key, value)
+			}
+			mSession.setRepeatingRequest(builder.build(), mSessionCaptureCallback, mHandler)
+		} catch (e: CameraAccessException) {
+			e.printStackTrace()
+		}
+	}
+	
+	private fun takePicture() {
+		mState = STATE_CAPTURE
+		try {
+			val builder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+			builder.addTarget(mImageReader.surface)
+			
+			mSession.stopRepeating()
+			mSession.abortCaptures()
+			mSession.capture(builder.build(), mSessionCaptureCallback, null)
+		} catch (e: CameraAccessException) {
+			e.printStackTrace()
+		}
+	}
 }
--- CameraProject/Camera/app/src/main/java/com/example/camera/MainActivity.kt
+++ CameraProject/Camera/app/src/main/java/com/example/camera/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -14,10 +20,56 @@ import java.net.Socket
 //todo 参数设置
 
 class MainActivity : AppCompatActivity() {
+	val permissions = arrayListOf(Manifest.permission.CAMERA)
+	
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-		//todo 统一权限处理
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			checkPermissions()
+		} else {
+			showPreview()
+		}
+	}
+	
+	@RequiresApi(Build.VERSION_CODES.M)
+	private fun checkPermissions() {
+		val needPermissions = ArrayList<String>()
+		for (permission in permissions) {
+			if (ContextCompat.checkSelfPermission(
+					this,
+					permission
+				) != PackageManager.PERMISSION_GRANTED
+			) {
+				needPermissions.add(permission)
+			}
+		}
+		if (needPermissions.isNotEmpty()) {
+			requestPermissions(needPermissions.toTypedArray(), 1)
+		} else {
+			showPreview()
+		}
+	}
+	
+	private fun showPreview() {
+		supportFragmentManager.beginTransaction().replace(R.id.replaceContent, CameraFragment())
+			.commit()
+	}
+	
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+		for (i in grantResults) {
+			if (i != PackageManager.PERMISSION_GRANTED) {
+				Toast.makeText(this, "缺少权限", Toast.LENGTH_SHORT).show()
+				System.exit(0)
+			}
+		}
+		showPreview()
 	}
 }
