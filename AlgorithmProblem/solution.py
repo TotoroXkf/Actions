62812fc21e57a6281cd39b24ad43f4c83d341f46
TotoroXkf <1131225133@qq.com>
2019-01-29 22:09:06 +0800

    Coding

--- AlgorithmProblem/solution.py
+++ AlgorithmProblem/solution.py
@@ -1,18 +1,22 @@
-from base import TreeNode
+from base import TreeNode, ListNode
 
 
 class Solution:
-    def sortedArrayToBST(self, nums: list):
-        length = len(nums)
-        if length == 0:
+    def sortedListToBST(self, head: ListNode):
+        if head is None:
             return None
-        if length == 1:
-            return nums[0]
-        index = length // 2
-        node = TreeNode(nums[index])
-        node.left = self.sortedArrayToBST(nums[0:index])
-        node.right = self.sortedArrayToBST(nums[index + 1:length])
+        if head.next is None:
+            return TreeNode(head.val)
+        pre_head = ListNode(0)
+        pre_head.next = head
+        pre, fast = pre_head, head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            pre = pre.next
+        mid = pre.next
+        node = TreeNode(mid.val)
+        if mid is not head:
+            pre.next = None
+            node.left = self.sortedListToBST(head)
+        node.right = self.sortedListToBST(mid.next)
         return node
-
-
-Solution().sortedArrayToBST([-10, -3, 0, 5, 9])
