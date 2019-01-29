eae179f5fb84ee27d59bdbafd0964c354335eed0
TotoroXkf <1131225133@qq.com>
2019-01-29 19:40:54 +0800

    Coding

--- AlgorithmProblem/solution.py
+++ AlgorithmProblem/solution.py
@@ -3,5 +3,16 @@ from base import TreeNode
 
 class Solution:
     def sortedArrayToBST(self, nums: list):
-        if not nums:
+        length = len(nums)
+        if length == 0:
             return None
+        if length == 1:
+            return nums[0]
+        index = length // 2
+        node = TreeNode(nums[index])
+        node.left = self.sortedArrayToBST(nums[0:index])
+        node.right = self.sortedArrayToBST(nums[index + 1:length])
+        return node
+
+
+Solution().sortedArrayToBST([-10, -3, 0, 5, 9])
