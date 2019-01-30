8104eb517bd4be8784925b6a73f28bba2456f6e8
TotoroXkf <1131225133@qq.com>
2019-01-30 12:22:30 +0800

    Coding

--- AlgorithmProblem/solution.py
+++ AlgorithmProblem/solution.py
@@ -2,30 +2,16 @@ from base import TreeNode, ListNode
 
 
 class Solution:
-    def flatten(self, root: TreeNode):
-        rebuild(root)
-
-
-def rebuild(root: TreeNode):
-    if root is None:
-        return None
-    if root.left is None and root.right is None:
-        return root
-    tail = root
-    left_node, right_node = root.left, root.right
-    root.left = None
-    root.right = rebuild(left_node)
-    while tail.right is not None:
-        tail = tail.right
-    tail.right = rebuild(right_node)
-    return root
-
-
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.left.left = TreeNode(3)
-root.left.right = TreeNode(4)
-root.right = TreeNode(5)
-root.right.right = TreeNode(6)
-root = rebuild(root)
-print("xxx")
+    def generate(self, rows: int):
+        result = []
+        for i in range(0, rows):
+            new_answer = []
+            for j in range(0, i + 1):
+                if j == 0:
+                    new_answer.append(1)
+                elif j == i:
+                    new_answer.append(1)
+                else:
+                    new_answer.append(result[i - 1][j] + result[i - 1][j - 1])
+            result.append(new_answer)
+        return result
