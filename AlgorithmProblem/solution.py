d15ed64f614f9abbff4a64e6922aa29905c45481
TotoroXkf <1131225133@qq.com>
2019-01-30 14:01:20 +0800

    Coding

--- AlgorithmProblem/solution.py
+++ AlgorithmProblem/solution.py
@@ -2,16 +2,31 @@ from base import TreeNode, ListNode
 
 
 class Solution:
-    def getRow(self, row: int):
-        result = [1 for i in range(0, row + 1)]
-        for i in range(2, row + 1):
-            pre = 1
-            for j in range(1, i):
-                value = pre + result[j]
-                pre = result[j]
-                result[j] = value
-        return result
+    def minimumTotal(self, triangle: list):
+        length = len(triangle)
+        dp = [0 for i in range(0, length)]
+        dp[0] = triangle[0][0]
+        min_value = dp[0]
+        for i in range(1, length):
+            min_value = 2147483647
+            new_list = []
+            for j in range(0, i + 1):
+                if j == 0:
+                    value = dp[j] + triangle[i][j]
+                elif j == i:
+                    value = dp[j - 1] + triangle[i][j]
+                else:
+                    value = min(dp[j - 1] + triangle[i][j], dp[j] + triangle[i][j])
+                new_list.append(value)
+                min_value = min(value, min_value)
+            dp = new_list
+        return min_value
 
 
-test = Solution().getRow(3)
-print(test)
+test = [
+    [2],
+    [3, 4],
+    [6, 5, 7],
+    [4, 1, 8, 3]
+]
+print(Solution().minimumTotal(test))
