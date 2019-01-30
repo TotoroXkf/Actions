0ad8221ae2ea7128ccc5487af60c0f168cba036a
TotoroXkf <1131225133@qq.com>
2019-01-30 11:05:17 +0800

    Coding

--- AlgorithmProblem/solution.py
+++ AlgorithmProblem/solution.py
@@ -2,21 +2,19 @@ from base import TreeNode, ListNode
 
 
 class Solution:
-    def sortedListToBST(self, head: ListNode):
-        if head is None:
-            return None
-        if head.next is None:
-            return TreeNode(head.val)
-        pre_head = ListNode(0)
-        pre_head.next = head
-        pre, fast = pre_head, head
-        while fast.next is not None and fast.next.next is not None:
-            fast = fast.next.next
-            pre = pre.next
-        mid = pre.next
-        node = TreeNode(mid.val)
-        if mid is not head:
-            pre.next = None
-            node.left = self.sortedListToBST(head)
-        node.right = self.sortedListToBST(mid.next)
-        return node
+    def pathSum(self, root: TreeNode, target: int):
+        result = []
+        dfs(root, target, [], result)
+        return result
+
+
+def dfs(root: TreeNode, target: int, nums: list, result: list):
+    if root is None:
+        return
+    target -= root.val
+    nums.append(root.val)
+    if root.left is None and root.right is None and target == 0:
+        result.append(nums)
+        return
+    dfs(root.left, target, nums[:], result)
+    dfs(root.right, target, nums[:], result)
