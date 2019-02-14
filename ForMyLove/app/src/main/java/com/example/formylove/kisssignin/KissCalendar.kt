88cc29d3932be27f86ab1dd4d9bcc87b7c01a766
TotoroXkf <1131225133@qq.com>
2019-02-14 22:55:01 +0800

    Coding

--- ForMyLove/app/src/main/java/com/example/formylove/kisssignin/KissCalendar.kt
+++ ForMyLove/app/src/main/java/com/example/formylove/kisssignin/KissCalendar.kt
@@ -1,18 +1,23 @@
 package com.example.formylove.kisssignin
 
 import android.content.Context
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 
 class KissCalendar : View {
+	private val rect = RectF(0f, 0f, 0f, 0f)
+	private var rectLength = 0f
+	
 	constructor(context: Context?) : super(context)
 	constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 	constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 	
+	
 	init {
 		val dm = resources.displayMetrics
-		Log.e("xkf123456789", "" + dm.widthPixels)
-		Log.e("xkf123456789", "" + dm.heightPixels)
+		val screenWidth = dm.widthPixels
+		rectLength = screenWidth.toFloat() / 7f
 	}
 }
