bab31f7521133534db9973023b11e21e3da46eb3
TotoroXkf <1131225133@qq.com>
2019-02-15 21:14:35 +0800

    Coding

--- ForMyLove/app/src/main/java/com/example/formylove/kisssignin/KissCalendar.kt
+++ ForMyLove/app/src/main/java/com/example/formylove/kisssignin/KissCalendar.kt
@@ -7,6 +7,9 @@ import android.util.Log
 import android.view.View
 
 class KissCalendar : View {
+	private var viewWidth = 0f
+	private var viewHeight = 0f
+	
 	private val rect = RectF(0f, 0f, 0f, 0f)
 	private var rectLength = 0f
 	
@@ -14,10 +17,27 @@ class KissCalendar : View {
 	constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 	constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 	
-	
 	init {
-		val dm = resources.displayMetrics
-		val screenWidth = dm.widthPixels
-		rectLength = screenWidth.toFloat() / 7f
+	}
+	
+	override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+		super.onSizeChanged(w, h, oldw, oldh)
+		viewWidth = w.toFloat()
+		viewHeight = w.toFloat()
+		rectLength = viewWidth / 7
+		rect.set(0f, 0f, rectLength, rectLength)
+	}
+	
+	fun moveRect() {
+		val left = rect.left
+		val top = rect.top
+		val right = rect.right
+		val bottom = rect.bottom
+		
+		if (Math.abs(viewWidth - right + rectLength) > 5) {
+			rect.set(left + rectLength, top, right + rectLength, bottom)
+		} else {
+			rect.set(0f, bottom, rectLength, bottom + rectLength)
+		}
 	}
 }
