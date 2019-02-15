3e7a4e69f7c56f50bddd66fa1b82ad0e40496efa
TotoroXkf <1131225133@qq.com>
2019-02-15 20:59:19 +0800

    Coding

--- DjangoStudy/DjangoStudy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from snippets import views
-
-urlpatterns = [
-    path('', include('snippets.urls'))
-]
--- FirstWebPage/mainJS.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$("#menuBtn").click(function (event) {
-	$(".menuContent").animate({
-		left: '0px',
-	}, 500);
-	$(".cover").fadeIn();
-});
-
