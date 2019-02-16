f7373b3dddb744d64e79da78b54f0e32e5100113
TotoroXkf <1131225133@qq.com>
2019-02-16 11:50:34 +0800

    Coding

--- ForMyLove/app/src/main/java/com/example/formylove/kisssignin/LoveShape.kt
+++ ForMyLove/app/src/main/java/com/example/formylove/kisssignin/LoveShape.kt
@@ -8,35 +8,10 @@ class LoveShape(private val rect: RectF) {
 	private val rate = 4 * (sqrt(2f) - 1) / 3
 	private val radius = (rect.right - rect.left) / 2
 	
-	private val points = ArrayList<Float>()
-	private val controlPoints = ArrayList<Float>()
+	private val points = FloatArray(8)
+	private val controlPoints = FloatArray(16)
 	private val path = Path()
 	private val paint = Paint()
-	
-	private var topPointX = 0f
-	private var topPointY = 0f
-	private var leftPointX = 0f
-	private var leftPointY = 0f
-	private var rightPointX = 0f
-	private var rightPointY = 0f
-	private var bottomPointX = 0f
-	private var bottomPointY = 0f
-	private var leftTopPointX1 = 0f
-	private var leftTopPointX2 = 0f
-	private var leftTopPointY1 = 0f
-	private var leftTopPointY2 = 0f
-	private var rightTopPointX1 = 0f
-	private var rightTopPointX2 = 0f
-	private var rightTopPointY1 = 0f
-	private var rightTopPointY2 = 0f
-	private var rightBottomPointX1 = 0f
-	private var rightBottomPointX2 = 0f
-	private var rightBottomPointY1 = 0f
-	private var rightBottomPointY2 = 0f
-	private var leftBottomPointX1 = 0f
-	private var leftBottomPointX2 = 0f
-	private var leftBottomPointY1 = 0f
-	private var leftBottomPointY2 = 0f
 	
 	init {
 		paint.isAntiAlias = true
@@ -45,71 +20,43 @@ class LoveShape(private val rect: RectF) {
 	}
 	
 	private fun setUpPoints() {
-		//左边的点
-		leftPointX = rect.left
-		leftPointY = (rect.top + rect.bottom) / 2
 		//上面的点
-		topPointX = (rect.left + rect.right) / 2
-		topPointY = rect.top
+		points[0] = (rect.left + rect.right) / 2
+		points[1] = rect.top
 		//右边的点
-		rightPointX = rect.right
-		rightPointY = (rect.top + rect.bottom) / 2
+		points[2] = rect.right
+		points[3] = (rect.top + rect.bottom) / 2
 		//底下的点
-		bottomPointX = (rect.left + rect.right) / 2
-		bottomPointY = rect.bottom
-		points.run {
-			add(topPointX)
-			add(topPointY)
-			add(rightPointX)
-			add(rightPointY)
-			add(bottomPointX)
-			add(bottomPointY)
-			add(leftPointX)
-			add(leftPointY)
-		}
+		points[4] = (rect.left + rect.right) / 2
+		points[5] = rect.bottom
+		//左边的点
+		points[6] = rect.left
+		points[7] = (rect.top + rect.bottom) / 2
 		
 		//左上的第一个控制点
-		leftTopPointX1 = leftPointX
-		leftTopPointY1 = leftPointY - radius * rate
+		controlPoints[0] = points[6]
+		controlPoints[1] = points[7] - radius * rate
 		//左上的第二个控制点
-		leftTopPointX2 = topPointX - radius * rate
-		leftTopPointY2 = topPointY
+		controlPoints[2] = points[0] - radius * rate
+		controlPoints[3] = points[1]
 		//右上的第一个控制点
-		rightTopPointX1 = topPointX + radius * rate
-		rightTopPointY1 = topPointY
+		controlPoints[4] = points[0] + radius * rate
+		controlPoints[5] = points[1]
 		//右上的第二个控制点
-		rightTopPointX2 = rightPointX
-		rightTopPointY2 = rightPointY - radius * rate
+		controlPoints[6] = points[2]
+		controlPoints[7] = points[3] - radius * rate
 		//右下的第一个控制点
-		rightBottomPointX1 = rightPointX
-		rightBottomPointY1 = rightPointY + radius * rate
+		controlPoints[8] = points[2]
+		controlPoints[9] = points[3] + radius * rate
 		//右下的第二个控制点
-		rightBottomPointX2 = bottomPointX + radius * rate
-		rightBottomPointY2 = bottomPointY
+		controlPoints[10] = points[4] + radius * rate
+		controlPoints[11] = points[5]
 		//左下的第一个控制点
-		leftBottomPointX1 = bottomPointX - radius * rate
-		leftBottomPointY1 = bottomPointY
+		controlPoints[12] = points[4] - radius * rate
+		controlPoints[13] = points[5]
 		//左下的第二个控制点
-		leftBottomPointX2 = leftPointX
-		leftBottomPointY2 = leftPointY + radius * rate
-		controlPoints.run {
-			add(leftTopPointX1)
-			add(leftTopPointY1)
-			add(leftTopPointX2)
-			add(leftTopPointY2)
-			add(rightTopPointX1)
-			add(rightTopPointY1)
-			add(rightTopPointX2)
-			add(rightTopPointY2)
-			add(rightBottomPointX1)
-			add(rightBottomPointY1)
-			add(rightBottomPointX2)
-			add(rightBottomPointY2)
-			add(leftBottomPointX1)
-			add(leftBottomPointY1)
-			add(leftBottomPointX2)
-			add(leftBottomPointY2)
-		}
+		controlPoints[14] = points[6]
+		controlPoints[15] = points[7] + radius * rate
 	}
 	
 	fun setRect(rectF: RectF) {
@@ -118,7 +65,7 @@ class LoveShape(private val rect: RectF) {
 	
 	fun drawLove(canvas: Canvas) {
 		path.reset()
-		path.moveTo(leftPointX, leftPointY)
+		path.moveTo(points[6], points[7])
 		var i = 0
 		var j = 0
 		for (k in 0..3) {
