405fc7dd8beb8f17c386b553c504af7b4199984a
TotoroXkf <1131225133@qq.com>
2019-02-25 16:18:04 +0800

    Coding

--- Prepare/app/src/main/java/com/xiakaifa/app/prepare/MainActivity.java
+++ Prepare/app/src/main/java/com/xiakaifa/app/prepare/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
         MyConstraintLayout myConstraintLayout = findViewById(R.id.myConstraintLayout);
         RecyclerView recyclerView  = findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(new MyAdapter());
+        MyAdapter adapter = new MyAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(RecyclerView.ViewHolder viewHolder, int position) {
+                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
     }
 }
