af26318ff5f3cca5c7f94e007d98fd54398cf729
TotoroXkf <1131225133@qq.com>
2019-03-21 21:11:52 +0800

    Coding

---
+++ flutter_first/lib/base_widget/CheckBoxTest.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxTest extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    
+    return null;
+  }
+  
+}
+
+class CheckBoxState extends State<CheckBoxTest>{
+  @override
+  Widget build(BuildContext context) {
+    
+    return null;
+  }
+}
