6c19464ab43c3c93eaf3778859bda7850f8c7691
TotoroXkf <1131225133@qq.com>
2019-03-24 19:34:33 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -8,15 +8,15 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter",
-      home: new Scaffold(
-        appBar: AppBar(
-          title: new Text("Flutter"),
-        ),
-        body: new MyWidget(),
-      ),
       theme: new ThemeData(
         primaryColor: Colors.lightBlue[800],
       ),
+      routes: {
+        '/first': (BuildContext context) => FirstPage(),
+        '/second': (BuildContext context) => SecondPage(),
+      },
+      initialRoute: '/first',
+      home: MyWidget(),
     );
   }
 }
@@ -29,67 +29,51 @@ class MyWidget extends StatefulWidget {
 }
 
 class MyWidgetState extends State<MyWidget> {
-  GlobalKey<FormState> globalKey = new GlobalKey<FormState>();
-  String userName;
-  String password;
-
-  void login() {
-    var loginForm = globalKey.currentState;
-    if (loginForm.validate()) {
-      loginForm.save();
-      print(userName);
-      print("-----");
-      print(password);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      children: <Widget>[
-        new Container(
-          padding: EdgeInsets.all(16.0),
-          child: new Form(
-            key: globalKey,
-            child: new Column(
-              children: <Widget>[
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    hintText: "请输入用户名",
-                  ),
-                  onSaved: (value) {
-                    userName = value;
-                  },
-                  onFieldSubmitted: (value) {},
-                ),
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    hintText: "请输入密码",
-                  ),
-                  onSaved: (value) {
-                    password = value;
-                  },
-                  obscureText: true,
-                  validator: (value) {
-                    return value.length < 6 ? "密码长度小于6位" : null;
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-        new SizedBox(
-          width: 340,
-          height: 42.0,
-          child: new RaisedButton(
-            onPressed: login,
-            child: new Text(
-              "登录",
-              style: new TextStyle(fontSize: 18.0),
-            ),
-          ),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('主页'),
+      ),
+      body: Text("主页"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('这是第一个页面'),
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/second');
+        },
+        child: new Text("跳转到第二个页面"),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('这是第二个页面'),
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/first');
+        },
+        child: new Text("跳转到第一个页面"),
+      ),
     );
   }
 }
