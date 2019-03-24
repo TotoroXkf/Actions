80bf0a7acc9ce40bf8d98b1e65d85148961603e9
TotoroXkf <1131225133@qq.com>
2019-03-24 19:49:07 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -9,71 +9,31 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Flutter",
       theme: new ThemeData(
-        primaryColor: Colors.lightBlue[800],
+        primaryColor: Colors.blue,
       ),
-      routes: {
-        '/first': (BuildContext context) => FirstPage(),
-        '/second': (BuildContext context) => SecondPage(),
-      },
-      initialRoute: '/first',
-      home: MyWidget(),
-    );
-  }
-}
-
-class MyWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyWidgetState();
-  }
-}
-
-class MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('主页'),
-      ),
-      body: Text("主页"),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
+      home: Scaffold(
+        appBar: appBar,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text('这是第一个页面'),
-      ),
-      body: RaisedButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/second');
-        },
-        child: new Text("跳转到第二个页面"),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text('这是第二个页面'),
-      ),
-      body: RaisedButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/first');
-        },
-        child: new Text("跳转到第一个页面"),
-      ),
-    );
-  }
-}
+Widget appBar = AppBar(
+  title: Text('AppBar'),
+  actions: <Widget>[
+    IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.search),
+      tooltip: '搜索',
+    ),
+    IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.add),
+      tooltip: '更多',
+    )
+  ],
+);
