a8ae66db1c84d51e26f553f86be1a968e6f68a3d
TotoroXkf <1131225133@qq.com>
2019-03-24 10:08:16 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -6,14 +6,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Flutter",
-        theme: new ThemeData(primarySwatch: Colors.blue),
-        home: new Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(title: Text("Flutter")),
-
-        ));
+      debugShowCheckedModeBanner: false,
+      title: "Flutter",
+      home: new MyHomePage(
+        title: "主题测试",
+      ),
+      theme: new ThemeData(
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+        brightness: Brightness.dark,
+      ),
+    );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text(
+            '带背景颜色的文本组件',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+      ),
+      floatingActionButton: new Theme(
+        data: Theme.of(context).copyWith(accentColor: Colors.grey),
+        child: new FloatingActionButton(
+          onPressed: null,
+          child: new Icon(
+            Icons.computer,
+          ),
+        ),
+      ),
+    );
+  }
+}
