a40f00a40a39db254b8bebc86a794944218a43f1
TotoroXkf <1131225133@qq.com>
2019-03-24 16:06:58 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void getWeatherData() async {
+    try {
+      HttpClient httpClient = new HttpClient();
+      HttpClientRequest request = await httpClient.getUrl(
+          Uri.parse("http://t.weather.sojson.com/api/weather/city/101030100"));
+      HttpClientResponse response = await request.close();
+      var result = await response.transform(utf8.decoder).join();
+      print(result);
+      httpClient.close();
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -15,11 +32,8 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
           child: new RaisedButton(
-            onPressed: () {
-              const url = "https://www.baidu.com";
-              launch(url);
-            },
-            child: new Text("打开百度"),
+            onPressed: getWeatherData,
+            child: new Text("http请求示例"),
           ),
         ),
       ),
@@ -27,39 +41,6 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.lightBlue[800],
         accentColor: Colors.cyan[600],
         brightness: Brightness.dark,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(title),
-      ),
-      body: new Center(
-        child: new Container(
-          color: Theme.of(context).accentColor,
-          child: new Text(
-            '带背景颜色的文本组件',
-            style: Theme.of(context).textTheme.title,
-          ),
-        ),
-      ),
-      floatingActionButton: new Theme(
-        data: Theme.of(context).copyWith(accentColor: Colors.grey),
-        child: new FloatingActionButton(
-          onPressed: null,
-          child: new Icon(
-            Icons.computer,
-          ),
-        ),
       ),
     );
   }
