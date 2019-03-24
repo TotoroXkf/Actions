12decbf61b8597bca73ca2879fe7fa9174768bc7
TotoroXkf <1131225133@qq.com>
2019-03-24 16:15:34 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:io';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  void getWeatherData() async {
-    try {
-      HttpClient httpClient = new HttpClient();
-      HttpClientRequest request = await httpClient.getUrl(
-          Uri.parse("http://t.weather.sojson.com/api/weather/city/101030100"));
-      HttpClientResponse response = await request.close();
-      var result = await response.transform(utf8.decoder).join();
-      print(result);
-      httpClient.close();
-    } catch (e) {
-      print(e);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -28,12 +10,30 @@ class MyApp extends StatelessWidget {
       title: "Flutter",
       home: new Scaffold(
         appBar: AppBar(
-          title: new Text("第三方包的使用"),
+          title: new Text("Flutter"),
         ),
         body: new Center(
-          child: new RaisedButton(
-            onPressed: getWeatherData,
-            child: new Text("http请求示例"),
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              border: new Border.all(
+                color: Colors.grey,
+                width: 8.0,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(8.0),
+              ),
+            ),
+            child: new Text(
+              "Flutter",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+              ),
+            ),
           ),
         ),
       ),
