92daf342ce5dc4fcb7eca5a160973caaa4b0794c
TotoroXkf <1131225133@qq.com>
2019-03-24 16:42:34 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -12,36 +12,84 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: new Text("Flutter"),
         ),
-        body: new Center(
-          child: Container(
-            width: 200.0,
-            height: 200.0,
-            decoration: new BoxDecoration(
-              color: Colors.white,
-              border: new Border.all(
-                color: Colors.grey,
-                width: 8.0,
-              ),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(8.0),
-              ),
-            ),
-            child: new Text(
-              "Flutter",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28.0,
-              ),
-            ),
-          ),
-        ),
+        body: new MyWidget(),
       ),
       theme: new ThemeData(
         primaryColor: Colors.lightBlue[800],
-        accentColor: Colors.cyan[600],
-        brightness: Brightness.dark,
       ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyWidgetState();
+  }
+}
+
+class MyWidgetState extends State<MyWidget> {
+  GlobalKey<FormState> globalKey = new GlobalKey<FormState>();
+  String userName;
+  String password;
+
+  void login() {
+    var loginForm = globalKey.currentState;
+    if (loginForm.validate()) {
+      loginForm.save();
+      print(userName);
+      print("-----");
+      print(password);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          padding: EdgeInsets.all(16.0),
+          child: new Form(
+            key: globalKey,
+            child: new Column(
+              children: <Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(
+                    hintText: "请输入用户名",
+                  ),
+                  onSaved: (value) {
+                    userName = value;
+                  },
+                  onFieldSubmitted: (value) {},
+                ),
+                new TextFormField(
+                  decoration: new InputDecoration(
+                    hintText: "请输入密码",
+                  ),
+                  onSaved: (value) {
+                    password = value;
+                  },
+                  obscureText: true,
+                  validator: (value) {
+                    return value.length < 6 ? "密码长度小于6位" : null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        new SizedBox(
+          width: 340,
+          height: 42.0,
+          child: new RaisedButton(
+            onPressed: login,
+            child: new Text(
+              "登录",
+              style: new TextStyle(fontSize: 18.0),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
