431857fc5e539f9baf47e0fdd0af64c6c5eb1b24
TotoroXkf <1131225133@qq.com>
2019-04-08 17:01:23 +0800

    Coding

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -3,70 +3,36 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<Tab> tabs = [
+    Tab(
+      text: '选项卡一',
+    ),
+    Tab(
+      text: '选项卡二',
+    ),
+  ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-//      home: Scaffold(
-//        appBar: AppBar(title: Text("Flutter")),
-//      ),
-      home: MyPage(),
-    );
-  }
-}
-
-class MyPage extends StatefulWidget {
-  MyPage({Key key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return MyPageState();
-  }
-}
-
-class MyPageState extends State<MyPage> {
-  int selectIndex = 1;
-  final widgetOptions = [
-    Text('Index 0: 信息'),
-    Text('Index 1: 通讯录'),
-    Text('Index 2: 发现'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Flutter")),
-      body: Center(
-        child: widgetOptions.elementAt(selectIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            title: Text("信息"),
+      home: DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter"),
+            bottom: TabBar(tabs: tabs),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            title: Text("通讯录"),
+          body: TabBarView(
+            children: tabs.map((Tab tab) {
+              return Center(child: Text(tab.text));
+            }).toList(),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text("发现"),
-          ),
-        ],
-        currentIndex: selectIndex,
-        fixedColor: Colors.deepPurple,
-        onTap: onClick,
+        ),
       ),
     );
-  }
-
-  void onClick(int index) {
-    setState(() {
-      selectIndex = index;
-    });
   }
 }
