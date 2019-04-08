c649684c333284a9859768d5f1a372fb331572d3
TotoroXkf <1131225133@qq.com>
2019-04-08 16:22:04 +0800

    Coding

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -5,14 +5,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(title: Text("Flutter")),
-          body: MyPage(),
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+//      home: Scaffold(
+//        appBar: AppBar(title: Text("Flutter")),
+//      ),
+      home: MyPage(),
+    );
   }
 }
 
@@ -26,8 +28,45 @@ class MyPage extends StatefulWidget {
 }
 
 class MyPageState extends State<MyPage> {
+  int selectIndex = 1;
+  final widgetOptions = [
+    Text('Index 0: 信息'),
+    Text('Index 1: 通讯录'),
+    Text('Index 2: 发现'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter")),
+      body: Center(
+        child: widgetOptions.elementAt(selectIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            title: Text("信息"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            title: Text("通讯录"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text("发现"),
+          ),
+        ],
+        currentIndex: selectIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: onClick,
+      ),
+    );
+  }
+
+  void onClick(int index) {
+    setState(() {
+      selectIndex = index;
+    });
   }
 }
