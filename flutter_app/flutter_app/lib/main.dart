4ba2a7855c0ec7e1e84ac1dc6e5695323f05e2cc
TotoroXkf <1131225133@qq.com>
2019-04-08 17:53:22 +0800

    Coding

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -2,53 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class ItemModel {
-  const ItemModel({this.title, this.icon});
-
-  final String title;
-  final IconData icon;
-}
-
-const List<ItemModel> items = [
-  ItemModel(title: "自驾", icon: Icons.directions_car),
-  ItemModel(title: "自行车", icon: Icons.directions_bike),
-  ItemModel(title: "轮船", icon: Icons.directions_boat),
-  ItemModel(title: "公交车", icon: Icons.directions_bus),
-  ItemModel(title: "火车", icon: Icons.directions_railway),
-  ItemModel(title: "步行", icon: Icons.directions_walk),
-];
-
-class SelectedView extends StatelessWidget {
-  ItemModel model;
-
-  @override
-  Widget build(BuildContext context) {
-    final textStyle = Theme.of(context).textTheme.display1;
-
-    return Card(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              model.icon,
-              size: 128.0,
-              color: textStyle.color,
-            ),
-            Text(
-              model.title,
-              style: textStyle,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  SelectedView({Key key, this.model}) : super(key: key);
-}
-
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -57,29 +10,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new DefaultTabController(
-          length: items.length,
-          child: Scaffold(
-            appBar: AppBar(
-                title: Text("Flutter"),
-                bottom: TabBar(
-                  isScrollable: true,
-                  tabs: items.map((ItemModel model) {
-                    return Tab(
-                      text: model.title,
-                      icon: Icon(model.icon),
-                    );
-                  }).toList(),
-                )),
-            body: TabBarView(
-              children: items.map((ItemModel model) {
-                return Padding(
-                  padding: EdgeInsets.all(10),
-                  child: SelectedView(model: model),
-                );
-              }).toList(),
-            ),
-          )),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text('大龙猫'),
+                accountEmail: Text('xkf123456789@gmail.com'),
+              ),
+              ListTile(title: Text('item1')),
+              ListTile(title: Text('item2')),
+              ListTile(title: Text('item3')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
