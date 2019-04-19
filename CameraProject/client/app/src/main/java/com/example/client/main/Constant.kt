990f2d51630ddd05a105840e063187007c0b8a66
TotoroXkf <1131225133@qq.com>
2019-04-19 12:42:25 +0800

    Coding

--- CameraProject/client/app/src/main/java/com/example/client/main/Constant.kt
+++ CameraProject/client/app/src/main/java/com/example/client/main/Constant.kt
@@ -11,19 +11,28 @@ const val OK = "ok"
  * 查看当前连接的设备
  */
 const val ACTION_DEVICE_COUNT = "count"
+
 /**
  * 拍摄
  */
 const val ACTION_CAPTURE = "capture"
+
 /**
  * 断开连接
  */
 const val ACTION_FINISH = "finish"
+
 /**
  * 回文测试
  */
 const val ACTION_ECHO = "echo"
+
 /**
  * 测量命令从发出到接收的延迟
  */
 const val ACTION_DELAY_TEST = "rtt"
+
+/**
+ * 服务端删除指定的注册的手机
+ */
+const val ACTION_REMOVE = "remove"
