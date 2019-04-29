b3856d84f8355f59e9e0d0217787536d7bbc614c
TotoroXkf <1131225133@qq.com>
2019-04-29 20:11:05 +0800

    Coding

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -11,50 +11,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
+      home: new Scaffold(
         appBar: AppBar(
           title: Text('Flutter'),
         ),
-        body: Stack(
-          children: <Widget>[
-            Align(
-              alignment: FractionalOffset.topLeft,
-              child: FlatButton(
-                onPressed: null,
-                child: Text('Button'),
-              ),
-            ),
-            Align(
-              alignment: FractionalOffset.topRight,
-              child: FlatButton(
-                color: Colors.blue,
-                onPressed: null,
-                child: Text('Button'),
-              ),
-            ),
-            Align(
-              alignment: FractionalOffset.center,
-              child: FlatButton(
-                onPressed: null,
-                child: Text('Button'),
-              ),
-            ),
-            Align(
-              alignment: FractionalOffset.bottomLeft,
-              child: FlatButton(
-                onPressed: null,
-                child: Text('Button'),
-              ),
-            ),
-            Align(
-              alignment: FractionalOffset.bottomRight,
-              child: FlatButton(
-                onPressed: null,
-                child: Text('Button'),
-              ),
-            ),
-          ],
+        // body:
+        body: new Center(
+          child: MyWidget(),
         ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = new SnackBar(content: new Text('这是一个SnackBar!'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      onLongPress: () {
+        print('长按');
+      },
+      child: Container(
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).buttonColor,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Text('测试按钮'),
       ),
     );
   }
