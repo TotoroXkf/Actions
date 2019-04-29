e71ce6196723f8a87ca2cec399a3d0c50e8ba465
TotoroXkf <1131225133@qq.com>
2019-04-29 20:57:33 +0800

    Coding

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -5,44 +5,38 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    List<String> items = List<String>.generate(30, (i) {
+      return "列表项 ${i + 1}";
+    });
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter'),
         ),
-        // body:
-        body: new Center(
-          child: MyWidget(),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Dismissible(
+              key: Key(item),
+              onDismissed: (direction) {
+                items.removeAt(index);
+              },
+              child: ListTile(
+                title: Text('$item'),
+              ),
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final snackBar = new SnackBar(content: new Text('这是一个SnackBar!'));
-        Scaffold.of(context).showSnackBar(snackBar);
-      },
-      onLongPress: () {
-        print('长按');
-      },
-      child: Container(
-        padding: EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color: Theme.of(context).buttonColor,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Text('测试按钮'),
-      ),
-    );
-  }
-}
+
+
