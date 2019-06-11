53810f2399ac223c13feb86ec3121e87710346ed
TotoroXkf <1131225133@qq.com>
2019-06-11 15:07:12 +0800

    Coding

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -14,54 +14,56 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter'),
         ),
-        body: MyWidget(),
+        body: Material(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: 250,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text('Demo'),
+                  background: Image.asset(
+                    './images/image.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding: EdgeInsets.all(8.0),
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 10.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 4.0),
+                  delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.cyan[100 * (index % 9)],
+                      child: Text('grid item $index'),
+                    );
+                  }, childCount: 50),
+                ),
+              ),
+              SliverFixedExtentList(
+                itemExtent: 50.0,
+                delegate: new SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    //创建列表项
+                    return new Container(
+                      alignment: Alignment.center,
+                      color: Colors.lightBlue[100 * (index % 9)],
+                      child: new Text('list item $index'),
+                    );
+                  },
+                  childCount: 50, //50个列表项
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
-  }
-}
-
-class MyWidget extends StatefulWidget {
-  @override
-  _MyWidgetState createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  List<IconData> data = [];
-
-  @override
-  void initState() {
-    loadData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        childAspectRatio: 1.0,
-      ),
-      itemCount: data.length,
-      itemBuilder: (BuildContext context, int index) {
-        if (index == data.length - 1) {
-          loadData();
-        }
-        return Icon(data[index]);
-      },
-    );
-  }
-
-  void loadData() {
-    Future.delayed(Duration(milliseconds: 200)).then((e) {
-      setState(() {
-        data.addAll([
-          Icons.ac_unit,
-          Icons.airport_shuttle,
-          Icons.all_inclusive,
-          Icons.beach_access,
-          Icons.cake,
-          Icons.free_breakfast,
-        ]);
-      });
-    });
   }
 }
