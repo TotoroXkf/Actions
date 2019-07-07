5172a49b5776802663624c76b1318ce5e4863465
TotoroXkf <1131225133@qq.com>
2019-07-07 21:09:14 +0800

    Coding

---
+++ SourceCodeTest/app/src/main/java/com/example/sourcecodetest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sourcecodetest;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View view = new View(this);
+    }
+}
