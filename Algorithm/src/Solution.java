805371bceefae58053eebaedfa67f37001051167
xiakaifa <1131225133@qq.com>
2019-07-21 09:36:50 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,35 +1,32 @@
+import java.util.HashMap;
+
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int upValue = 0;
-        ListNode node1 = l1;
-        ListNode node2 = l2;
-        ListNode result = null;
-        ListNode tail = null;
-        while (node1 != null || node2 != null) {
-            int count = (node1 == null ? 0 : node1.val) + (node2 == null ? 0 : node2.val)+upValue;
-            if (count >= 10) {
-                count -= 10;
-                upValue = 1;
-            } else {
-                upValue = 0;
-            }
-            if (result == null) {
-                result = new ListNode(count);
-                tail = result;
-            } else {
-                tail.next = new ListNode(count);
-                tail = tail.next;
-            }
-            if (node1 != null) {
-                node1 = node1.next;
-            }
-            if (node2 != null) {
-                node2 = node2.next;
-            }
+    public int lengthOfLongestSubstring(String s) {
+        if (s.isEmpty()) {
+            return 0;
         }
-        if (upValue == 1) {
-            tail.next = new ListNode(1);
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        int startIndex = 0;
+        int result = 1;
+        hashMap.put(s.charAt(0), 0);
+        for (int i = 1; i < s.length(); i++) {
+            if (!hashMap.containsKey(s.charAt(i))) {
+                result = Math.max(result, i - startIndex + 1);
+                hashMap.put(s.charAt(i),i);
+                continue;
+            }
+            int targetIndex = hashMap.get(s.charAt(i));
+            for (int j = startIndex; j < targetIndex; j++) {
+                hashMap.remove(s.charAt(j));
+            }
+            hashMap.put(s.charAt(i),i);
+            startIndex = targetIndex+1;
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(new Solution().lengthOfLongestSubstring(s));
     }
 }
