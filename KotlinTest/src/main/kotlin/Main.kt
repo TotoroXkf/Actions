ce9b623d6a062f0018662a03169856b6a4e7fb8d
xiakaifa <1131225133@qq.com>
2019-07-21 15:54:57 +0800

    Coding

--- KotlinTest/src/main/kotlin/Main.kt
+++ KotlinTest/src/main/kotlin/Main.kt
@@ -4,15 +4,21 @@ import kotlinx.coroutines.channels.SendChannel
 import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking<Unit> {
-    val request = launch {
-        repeat(3) { i ->
-            launch {
-                delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
-                println("Coroutine $i is done")
-            }
-        }
-        println("request: I'm done and I don't explicitly join my children that are still active")
+    launch {
+        kotlinx.coroutines.delay(1000)
+        println("World")
     }
-    request.join()
+    println("Hello")
 }
 
+//fun main() {
+//    GlobalScope.launch {
+//        launch {
+//            kotlinx.coroutines.delay(1000)
+//            println("World")
+//        }
+//        println("Hello")
+//    }
+//
+//    Thread.sleep(2000)
+//}
