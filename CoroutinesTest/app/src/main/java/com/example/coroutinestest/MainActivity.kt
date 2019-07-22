a2276a34df724ca6587b83dd40e608f20af72180
xiakaifa <1131225133@qq.com>
2019-07-22 16:16:10 +0800

    Coding

--- CoroutinesTest/app/src/main/java/com/example/coroutinestest/MainActivity.kt
+++ CoroutinesTest/app/src/main/java/com/example/coroutinestest/MainActivity.kt
@@ -4,27 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Contacts
 import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.Main) {
+        launch {
             textView.text = ""
+            progressBar.visibility = View.VISIBLE
 
-            val client = OkHttpClient()
-            val request = Request.Builder().url("https://api.github.com/users/totoroXkf").build()
-            val result = async(Dispatchers.IO) {
-                val response = client.newCall(request).execute()
-                response.body!!.string()
-            }
-            textView.text = result.await()
+            val result = loadData()
+
+            progressBar.visibility = View.GONE
+            textView.text = result
         }
+    }
+
+    private suspend fun loadData() = withContext(Dispatchers.IO) {
+        val client = OkHttpClient()
+        val request = Request.Builder().url("https://api.github.com/users/totoroXkf").build()
+        val response = client.newCall(request).execute()
+        response.body!!.string()
     }
 }
