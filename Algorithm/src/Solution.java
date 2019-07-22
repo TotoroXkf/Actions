8e4870328e46b14beb8383b75937492feb7546f6
xiakaifa <1131225133@qq.com>
2019-07-22 16:16:30 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,35 +1,16 @@
 class Solution {
-    public int myAtoi(String str) {
-        int i = 0;
-        for (; i < str.length() && str.charAt(i) == ' '; i++) ;
-        if (i == str.length()) {
-            return 0;
-        }
-        boolean isNav = false;
-        if (str.charAt(i) == '-') {
-            isNav = true;
-        }
+    public int maxArea(int[] height) {
         int result = 0;
-        if (isNav || str.charAt(i) == '+') {
-            i++;
-        }
-        for (; i < str.length() && str.charAt(i) == '0'; i++) ;
-        while (i < str.length() && Character.isDigit(str.charAt(i))) {
-            int value = str.charAt(i) - 48;
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && value > 7)) {
-                return Integer.MAX_VALUE;
+        int start = 0;
+        int end = height.length - 1;
+        while (start < end) {
+            int area = (end - start) * Math.min(height[start], height[end]);
+            result = Math.max(result, area);
+            if(height[start]<height[end]){
+                start++;
+            }else {
+                end--;
             }
-            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && value == 9)) {
-                return Integer.MIN_VALUE;
-            }
-            if (isNav) {
-                value *= -1;
-                if (result > 0) {
-                    result *= -1;
-                }
-            }
-            result = result * 10 + value;
-            i++;
         }
         return result;
     }
--- KotlinTest/src/main/kotlin/Main.kt
+++ KotlinTest/src/main/kotlin/Main.kt
@@ -1,24 +1,21 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.SendChannel
+import java.lang.ArithmeticException
+import java.lang.Exception
+import java.lang.IndexOutOfBoundsException
 import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking<Unit> {
-    launch {
-        kotlinx.coroutines.delay(1000)
-        println("World")
-    }
-    println("Hello")
-}
 
-//fun main() {
-//    GlobalScope.launch {
-//        launch {
-//            kotlinx.coroutines.delay(1000)
-//            println("World")
-//        }
-//        println("Hello")
-//    }
-//
-//    Thread.sleep(2000)
-//}
+    val job = launch {
+        println("1")
+        yield()
+        println("2")
+    }
+
+    println("3")
+    println("4")
+    job.join()
+    println("5")
+}
