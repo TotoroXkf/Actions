23c0a7b9d56f2e5283ba4e0445534e8993c80e59
xiakaifa <xiakaifa@keep.com>
2019-08-04 16:41:50 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -27,65 +27,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _textEditingController1 = new TextEditingController();
-  TextEditingController _textEditingController2 = new TextEditingController();
   GlobalKey _globalKey = new GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Form(
-        key: _globalKey,
-        autovalidate: true,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-                autofocus: true,
-                controller: _textEditingController1,
-                decoration: InputDecoration(
-                    labelText: "用户名",
-                    hintText: "用户名或邮箱",
-                    icon: Icon(Icons.person)),
-                // 校验用户名
-                validator: (v) {
-                  return v.trim().length > 0 ? null : "用户名不能为空";
-                }),
-            TextFormField(
-                controller: _textEditingController2,
-                decoration: InputDecoration(
-                    labelText: "密码",
-                    hintText: "您的登录密码",
-                    icon: Icon(Icons.lock)),
-                obscureText: true,
-                //校验密码
-                validator: (v) {
-                  return v.trim().length > 5 ? null : "密码不能少于6位";
-                }),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 28, 0, 0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      child: Text('登陆'),
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      onPressed: () {
-                        if ((_globalKey.currentState as FormState).validate()) {
-                          print("pass");
-                        } else {
-                          print("not");
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return null;
   }
 }
