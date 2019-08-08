9820b17e168699888c82f3605bd2c85799308764
xiakaifa <xiakaifa@keep.com>
2019-08-08 12:09:00 +0800

    coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:collection';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -30,85 +27,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _top = 0.0;
+  double _left = 0.0;
+  var _leftB = 100.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () async {
-          var result = await showMyDialog();
-          print(result);
-        },
-      ),
-    );
-  }
-
-  Future<bool> showMyDialog() {
-    return showCustomDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("提示"),
-          content: Text("您确定要删除当前文件吗?"),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("取消"),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            FlatButton(
-              child: Text("删除"),
-              onPressed: () {
-                // 执行删除操作
-                Navigator.of(context).pop(true);
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          top: 80.0,
+          left: _leftB,
+          child: Listener(
+            onPointerDown: (details) {
+              print("down");
+            },
+            onPointerUp: (details) {
+              //会触发
+              print("up");
+            },
+            child: GestureDetector(
+              child: CircleAvatar(child: Text("B")),
+              onHorizontalDragUpdate: (DragUpdateDetails details) {
+                setState(() {
+                  _leftB += details.delta.dx;
+                });
+              },
+              onHorizontalDragEnd: (details) {
+                print("onHorizontalDragEnd");
               },
             ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<T> showCustomDialog<T>({
-    @required BuildContext context,
-    bool barrierDismissible = true,
-    WidgetBuilder builder,
-  }) {
-    final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
-    return showGeneralDialog(
-      context: context,
-      pageBuilder: (BuildContext buildContext, Animation<double> animation,
-          Animation<double> secondaryAnimation) {
-        final Widget pageChild = Builder(builder: builder);
-        return SafeArea(
-          child: Builder(
-            builder: (BuildContext context) {
-              return theme != null
-                  ? Theme(data: theme, child: pageChild)
-                  : pageChild;
-            },
           ),
-        );
-      },
-      barrierDismissible: barrierDismissible,
-      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-      barrierColor: Colors.black87,
-      // 自定义遮罩颜色
-      transitionDuration: const Duration(milliseconds: 150),
-      transitionBuilder: _buildMaterialDialogTransitions,
-    );
-  }
-
-  Widget _buildMaterialDialogTransitions(
-      BuildContext context,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      Widget child) {
-    // 使用缩放动画
-    return ScaleTransition(
-      scale: CurvedAnimation(
-        parent: animation,
-        curve: Curves.easeOut,
-      ),
-      child: child,
+        ),
+      ],
     );
   }
 }
