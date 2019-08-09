f4dea614ed15f1a76e2d8d2e2500d9baf01ed67c
xiakaifa <xiakaifa@keep.com>
2019-08-09 18:34:43 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -27,106 +27,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  AnimationController _animationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-  }
-
-  Future<Null> _playAnimation() async {
-    try {
-      await _animationController.forward().orCancel;
-      await _animationController.reverse().orCancel;
-    } on TickerCanceled {}
-  }
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: () {
-        _playAnimation();
-      },
-      child: Center(
-        child: Container(
-          width: 300.0,
-          height: 300.0,
-          decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.1),
-            border: Border.all(
-              color: Colors.black.withOpacity(0.5),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              var tween = Tween<Offset>(begin: Offset(0, 0), end: Offset(1, 0));
+              return SlideTransition(
+                child: child,
+                position: tween.animate(animation),
+              );
+            },
+            child: Text(
+              '$_count',
+              //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
+              key: ValueKey<int>(_count),
+              style: Theme.of(context).textTheme.display1,
             ),
           ),
-          child: StaggerAnimation(
-            controller: _animationController,
+          RaisedButton(
+            child: const Text(
+              '+1',
+            ),
+            onPressed: () {
+              setState(() {
+                _count += 1;
+              });
+            },
           ),
-        ),
+        ],
       ),
-    );
-  }
-}
-
-class StaggerAnimation extends StatelessWidget {
-  final Animation<double> controller;
-  Animation<double> height;
-  Animation<EdgeInsets> padding;
-  Animation<Color> color;
-
-  StaggerAnimation({Key key, this.controller}) : super(key: key) {
-    height = Tween(begin: 0.0, end: 300.0).animate(
-      CurvedAnimation(
-        curve: Interval(
-          0.0,
-          0.6,
-          curve: Curves.ease,
-        ),
-        parent: controller,
-      ),
-    );
-
-    color = ColorTween(
-      begin: Colors.green,
-      end: Colors.red,
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Interval(
-          0.0,
-          0.6,
-          curve: Curves.ease,
-        ),
-      ),
-    );
-
-    padding = Tween(
-      begin: EdgeInsets.only(left: 0.0),
-      end: EdgeInsets.only(left: 100.0),
-    ).animate(
-      CurvedAnimation(
-          curve: Interval(0.6, 1.0, curve: Curves.ease), parent: controller),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (BuildContext context, Widget child) {
-        return Container(
-          alignment: Alignment.bottomCenter,
-          padding: padding.value,
-          child: Container(
-            color: color.value,
-            width: 50,
-            height: height.value,
-          ),
-        );
-      },
     );
   }
 }
