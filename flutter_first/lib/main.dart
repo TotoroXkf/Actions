aa9a1e18bfc675d1d99aa4b6d881756b73134fbe
xiakaifa <xiakaifa@keep.com>
2019-08-09 14:57:45 +0800

    Coding

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -26,39 +26,107 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+  }
+
+  Future<Null> _playAnimation() async {
+    try {
+      await _animationController.forward().orCancel;
+      await _animationController.reverse().orCancel;
+    } on TickerCanceled {}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            PageRouteBuilder(
-                transitionDuration: Duration(milliseconds: 500),
-                pageBuilder: (BuildContext context, Animation animation,
-                    Animation secondaryAnimation) {
-                  return new FadeTransition(
-                    opacity: animation,
-                    child: NewRoute(),
-                  );
-                }),
-          );
-        },
-        child: Text('第一个页面'),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        _playAnimation();
+      },
+      child: Center(
+        child: Container(
+          width: 300.0,
+          height: 300.0,
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.1),
+            border: Border.all(
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+          child: StaggerAnimation(
+            controller: _animationController,
+          ),
+        ),
       ),
     );
   }
 }
 
-class NewRoute extends StatelessWidget {
+class StaggerAnimation extends StatelessWidget {
+  final Animation<double> controller;
+  Animation<double> height;
+  Animation<EdgeInsets> padding;
+  Animation<Color> color;
+
+  StaggerAnimation({Key key, this.controller}) : super(key: key) {
+    height = Tween(begin: 0.0, end: 300.0).animate(
+      CurvedAnimation(
+        curve: Interval(
+          0.0,
+          0.6,
+          curve: Curves.ease,
+        ),
+        parent: controller,
+      ),
+    );
+
+    color = ColorTween(
+      begin: Colors.green,
+      end: Colors.red,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.0,
+          0.6,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+
+    padding = Tween(
+      begin: EdgeInsets.only(left: 0.0),
+      end: EdgeInsets.only(left: 100.0),
+    ).animate(
+      CurvedAnimation(
+          curve: Interval(0.6, 1.0, curve: Curves.ease), parent: controller),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () {},
-        child: Text('第二个页面'),
-      ),
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (BuildContext context, Widget child) {
+        return Container(
+          alignment: Alignment.bottomCenter,
+          padding: padding.value,
+          child: Container(
+            color: color.value,
+            width: 50,
+            height: height.value,
+          ),
+        );
+      },
     );
   }
 }
