d88607ea19650e1e727009bb2e994e908619986f
xiakaifa <xiakaifa@keep.com>
2019-08-11 08:15:26 +0800

    Coding

--- flutter_first/lib/route.dart
+++ flutter_first/lib/route.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:convert' as prefix0;
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
@@ -8,74 +11,166 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = new TextEditingController();
-  IOWebSocketChannel channel;
-  String _text = "";
-
-  @override
-  void initState() {
-    super.initState();
-    channel = new IOWebSocketChannel.connect('ws://echo.websocket.org');
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          new Form(
-            child: new TextFormField(
-              controller: _controller,
-              decoration: new InputDecoration(labelText: 'Send a message'),
-            ),
-          ),
-          StreamBuilder(
-            stream: channel.stream,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                print('error');
-              } else if (snapshot.hasData) {
-                _text = "echo: " + snapshot.data;
-              }
-
-              return new Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: new Text(_text),
-              );
-            },
-          ),
-          // Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: RaisedButton(
-          //     onPressed: (){
-          //       _sendMessage();
-          //     },
-          //   ),
-          // ),
-          Container(
-            width: double.infinity,
-            child: RaisedButton(
-              onPressed: () {
-                _sendMessage();
-              },
-            ),
-          ),
-        ],
+    Dio dio = new Dio();
+    return Center(
+      child: FutureBuilder(
+        future: dio.get("https://api.github.com/users/TotoroXkf"),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Response response = snapshot.data;
+            Info info = Info.fromJson(response.data);
+            return Text(info.id.toString());
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
 
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      channel.sink.add(_controller.text);
-    }
+  Future<String> loadData() async {
+    Dio dio = new Dio();
+    var response = await dio.get("https://api.github.com/users/TotoroXkf");
+    print(response.data);
+    return response.data;
+  }
+}
+
+class Info {
+  String login;
+  int id;
+  String nodeId;
+  String avatarUrl;
+  String gravatarId;
+  String url;
+  String htmlUrl;
+  String followersUrl;
+  String followingUrl;
+  String gistsUrl;
+  String starredUrl;
+  String subscriptionsUrl;
+  String organizationsUrl;
+  String reposUrl;
+  String eventsUrl;
+  String receivedEventsUrl;
+  String type;
+  bool siteAdmin;
+  String name;
+  Null company;
+  String blog;
+  Null location;
+  Null email;
+  Null hireable;
+  String bio;
+  int publicRepos;
+  int publicGists;
+  int followers;
+  int following;
+  String createdAt;
+  String updatedAt;
+
+  Info(
+      {this.login,
+      this.id,
+      this.nodeId,
+      this.avatarUrl,
+      this.gravatarId,
+      this.url,
+      this.htmlUrl,
+      this.followersUrl,
+      this.followingUrl,
+      this.gistsUrl,
+      this.starredUrl,
+      this.subscriptionsUrl,
+      this.organizationsUrl,
+      this.reposUrl,
+      this.eventsUrl,
+      this.receivedEventsUrl,
+      this.type,
+      this.siteAdmin,
+      this.name,
+      this.company,
+      this.blog,
+      this.location,
+      this.email,
+      this.hireable,
+      this.bio,
+      this.publicRepos,
+      this.publicGists,
+      this.followers,
+      this.following,
+      this.createdAt,
+      this.updatedAt});
+
+  Info.fromJson(Map<String, dynamic> json) {
+    login = json['login'];
+    id = json['id'];
+    nodeId = json['node_id'];
+    avatarUrl = json['avatar_url'];
+    gravatarId = json['gravatar_id'];
+    url = json['url'];
+    htmlUrl = json['html_url'];
+    followersUrl = json['followers_url'];
+    followingUrl = json['following_url'];
+    gistsUrl = json['gists_url'];
+    starredUrl = json['starred_url'];
+    subscriptionsUrl = json['subscriptions_url'];
+    organizationsUrl = json['organizations_url'];
+    reposUrl = json['repos_url'];
+    eventsUrl = json['events_url'];
+    receivedEventsUrl = json['received_events_url'];
+    type = json['type'];
+    siteAdmin = json['site_admin'];
+    name = json['name'];
+    company = json['company'];
+    blog = json['blog'];
+    location = json['location'];
+    email = json['email'];
+    hireable = json['hireable'];
+    bio = json['bio'];
+    publicRepos = json['public_repos'];
+    publicGists = json['public_gists'];
+    followers = json['followers'];
+    following = json['following'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
   }
 
-  @override
-  void dispose() {
-    channel.sink.close();
-    super.dispose();
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['login'] = this.login;
+    data['id'] = this.id;
+    data['node_id'] = this.nodeId;
+    data['avatar_url'] = this.avatarUrl;
+    data['gravatar_id'] = this.gravatarId;
+    data['url'] = this.url;
+    data['html_url'] = this.htmlUrl;
+    data['followers_url'] = this.followersUrl;
+    data['following_url'] = this.followingUrl;
+    data['gists_url'] = this.gistsUrl;
+    data['starred_url'] = this.starredUrl;
+    data['subscriptions_url'] = this.subscriptionsUrl;
+    data['organizations_url'] = this.organizationsUrl;
+    data['repos_url'] = this.reposUrl;
+    data['events_url'] = this.eventsUrl;
+    data['received_events_url'] = this.receivedEventsUrl;
+    data['type'] = this.type;
+    data['site_admin'] = this.siteAdmin;
+    data['name'] = this.name;
+    data['company'] = this.company;
+    data['blog'] = this.blog;
+    data['location'] = this.location;
+    data['email'] = this.email;
+    data['hireable'] = this.hireable;
+    data['bio'] = this.bio;
+    data['public_repos'] = this.publicRepos;
+    data['public_gists'] = this.publicGists;
+    data['followers'] = this.followers;
+    data['following'] = this.following;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
   }
 }
