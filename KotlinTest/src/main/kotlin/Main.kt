1939b74ba640dc8903ede473db5d29850e7c15d1
xiakaifa <xiakaifa@keep.com>
2019-08-13 11:04:39 +0800

    Coding

--- KotlinTest/src/main/kotlin/Main.kt
+++ KotlinTest/src/main/kotlin/Main.kt
@@ -1,15 +1,22 @@
+import kotlin.coroutines.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import kotlin.system.measureTimeMillis
 
-fun main() {
-    GlobalScope.launch {
-        kotlinx.coroutines.delay(500)
-        println("xkf")
+fun main() = runBlocking {
+    var count = 0;
+    //使用预置的线程池
+    withContext(Dispatchers.Default) {
+        val n = 10
+        val k = 10
+        repeat(n) {
+            launch {
+                //每一次都会从线程池里取一个线程
+                repeat(k) {
+                    //相当于多线程并发
+                    println(Thread.currentThread().name)
+                    count++
+                }
+            }
+        }
     }
-    GlobalScope.launch {
-        kotlinx.coroutines.delay(100)
-        println("zs")
-    }
+    println(count)
 }
