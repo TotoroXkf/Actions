e7dfb8a4ff8b0ded233824fce11425eb82d9ac16
xiakaifa <xiakaifa@keep.com>
2019-10-11 23:43:46 +0800

    Coding

--- KotlinTest/src/Test.kt
+++ KotlinTest/src/Test.kt
@@ -1,4 +1,5 @@
 import java.time.LocalDate
+import kotlin.reflect.KProperty
 
 operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> = object : Iterator<LocalDate> {
     var current = start
@@ -10,10 +11,31 @@ operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> = object : I
     }
 }
 
-fun main() {
-    val newYear = LocalDate.ofYearDay(2019, 100)
-    val daysOff = newYear.minusDays(20)..newYear
-    for (day in daysOff) {
-        println(day)
+class Point(val x: Int, val y: Int) {
+    operator fun component1() = x
+    operator fun component2() = y
+}
+
+class Foo {
+    var p by Delegate()
+}
+
+class Delegate() {
+    var value = 0
+
+    operator fun getValue(foo: Foo, property: KProperty<*>): Int {
+        return value
     }
+
+    operator fun setValue(foo: Foo, property: KProperty<*>, newValue: Int) {
+        value = newValue
+    }
+}
+
+
+fun main() {
+    val foo = Foo()
+    println(foo.p)
+    foo.p = 12
+    println(foo.p)
 }
