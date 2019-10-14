08f87f85aa9cc4469fc4deb4b9ece8326de17bf3
xiakaifa <xiakaifa@keep.com>
2019-10-14 19:53:56 +0800

    Coding

--- KotlinTest/src/Test.kt
+++ KotlinTest/src/Test.kt
@@ -1,46 +1,14 @@
-import java.time.LocalDate
-import kotlin.reflect.KProperty
+import java.util.*
 
-//operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> = object : Iterator<LocalDate> {
-//    var current = start
-//
-//    override fun hasNext(): Boolean = current < endInclusive
-//
-//    override fun next(): LocalDate = current.apply {
-//        current = plusDays(1)
-//    }
-//}
-//
-//class Point(val x: Int, val y: Int) {
-//    operator fun component1() = x
-//    operator fun component2() = y
-//}
-//
-//class Foo {
-//    var p by Delegate()
-//}
-//
-//class Delegate() {
-//    var value = 0
-//
-//    operator fun getValue(foo: Foo, property: KProperty<*>): Int {
-//        return value
-//    }
-//
-//    operator fun setValue(foo: Foo, property: KProperty<*>, newValue: Int) {
-//        value = newValue
-//    }
-//}
-
-inline fun someMethod(noinline func: (Int) -> Int) {
-    val x = func(1)
-    println("xkf")
-    println(x)
+inline fun <reified T> isA(value: Any) {
+    if (value is T) {
+        println("success")
+    } else {
+        println("error")
+    }
 }
 
 fun main() {
-    someMethod { value: Int ->
-        val x = value * 2
-        x
-    }
+    isA<String>(1)
+    isA<Calendar>(1)
 }
