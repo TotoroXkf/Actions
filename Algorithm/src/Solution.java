1b092368b2bf7080d8b353faa902b0969bfceba8
xiakaifa <xkf123456789@gmail.com>
2019-10-27 22:31:30 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,5 +1,36 @@
 class Solution {
-    public boolean checkSubarraySum(int[] nums, int k) {
-        
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        boolean hasChange = false;
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+                continue;
+            }
+            if (hasChange) {
+                return false;
+            }
+            if (j - i == 1) {
+                return true;
+            }
+            if (s.charAt(i) == s.charAt(j - 1)) {
+                j--;
+                hasChange = true;
+            } else if (s.charAt(j) == s.charAt(i + 1)) {
+                i++;
+                hasChange = true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "ebcbbececabbacecbbcbe";
+        System.out.println(solution.validPalindrome(str));
     }
 }
