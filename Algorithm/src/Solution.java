022a017dd72d3c34dd3c088431cdaed0aebf73ae
xiakaifa <xkf123456789@gmail.com>
2019-11-01 09:37:33 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,24 +1,5 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 class Solution {
-    public List<Integer[]> findTwoSideMInValuePosition(int[] array) {
-        ArrayList<Integer[]> result = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            result.add(new Integer[]{-1, -1});
-        }
-        LinkedList<Integer> stack = new LinkedList<>();
-        for (int i = 0; i < array.length; i++) {
-            while (!stack.isEmpty() && array[stack.peek()] > array[i]) {
-                int index = stack.pop();
-                result.get(index)[1] = i;
-            }
-            if (!stack.isEmpty()) {
-                result.get(i)[0] = stack.peek();
-            }
-            stack.push(i);
-        }
-        return result;
+    public int removeElement(int[] nums, int val) {
+        
     }
 }
