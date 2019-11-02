efbab988e1345a6a61c7d70f9a187f4d0478d602
xiakaifa <xkf123456789@gmail.com>
2019-11-02 11:17:05 +0800

    Coding

--- Algorithm/src/Run.java
+++ Algorithm/src/Run.java
@@ -1,7 +1,6 @@
 public class Run {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arrays = new int[]{3,4,1,5,6,2,7};
-        solution.findTwoSideMInValuePosition(arrays);
+        solution.countAndSay(5);
     }
 }
--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Solution {
-    public int removeElement(int[] nums, int val) {
-        
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        ArrayList<Integer[]> numberMap = new ArrayList<>();
+        int i = 2;
+        StringBuilder stringBuilder = new StringBuilder("1");
+        while (i <= n) {
+            numberMap.clear();
+            int target = -1;
+            int count = 0;
+            char[] numbers = stringBuilder.toString().toCharArray();
+            for (char number : numbers) {
+                if ((number - '0') != target) {
+                    if (target != -1) {
+                        numberMap.add(new Integer[]{target, count});
+                    }
+                    target = number - '0';
+                    count = 1;
+                } else {
+                    count++;
+                }
+            }
+            numberMap.add(new Integer[]{target, count});
+            stringBuilder.delete(0, stringBuilder.length());
+            for (Integer[] values : numberMap) {
+                stringBuilder.append(values[1]);
+                stringBuilder.append(values[0]);
+            }
+            i++;
+        }
+        return stringBuilder.toString();
     }
 }
