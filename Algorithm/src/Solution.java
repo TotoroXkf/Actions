32366675079ddb1f2fc84e8957da12f5af8be9b3
xiakaifa <xkf123456789@gmail.com>
2019-11-05 10:10:54 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,14 +1,5 @@
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        if (head.next == null) {
-            return head;
-        }
-        ListNode temp = head.next;
-        head.next = swapPairs(temp.next);
-        temp.next = head;
-        return temp;
+    public int divide(int dividend, int divisor) {
+        return dividend/dividend;
     }
 }
---
+++ AndroidTest/app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.androidtest
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
