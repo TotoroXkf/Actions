fa6a7a4d7556f84c8171f25dba67914828fc5025
xiakaifa <xkf123456789@gmail.com>
2019-11-09 23:36:50 +0800

    Coding

--- AndroidTest/app/src/main/java/com/example/androidtest/MainActivity.kt
+++ AndroidTest/app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -1,26 +1,26 @@
 package com.example.androidtest
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.graphics.PixelFormat
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Handler
-import android.view.Gravity
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.RemoteViews
-import androidx.annotation.RequiresApi
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissionCheck =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+        }
+
+        val textView: TextView? = null
+        textView!!.text = ""
     }
 }
