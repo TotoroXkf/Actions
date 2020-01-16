9201d9e5a2813c66e8ad2c76ea8aec811ddc84c7
xiakaifa <xkf123456789@gmail.com>
2020-01-16 10:20:25 +0800

    no message

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -2,40 +2,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        if (intervals.length == 0) {
-            return new int[0][0];
-        }
-        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
-        int[][] result = new int[intervals.length][2];
-        int currentIndex = 0;
-        int[] currentInterval = new int[2];
-        currentInterval[0] = intervals[0][0];
-        currentInterval[1] = intervals[0][1];
-        for (int i = 1; i < intervals.length; i++) {
-            int value1 = intervals[i][0];
-            int value2 = intervals[i][1];
-            if (value1 <= currentInterval[1]) {
-                currentInterval[1] = Math.max(currentInterval[1], value2);
-            } else {
-                result[currentIndex++] = currentInterval;
-                currentInterval = new int[2];
-                currentInterval[0] = intervals[i][0];
-                currentInterval[1] = intervals[i][1];
-            }
-        }
-        result[currentIndex++] = currentInterval;
-        result = Arrays.copyOf(result, currentIndex);
-        return result;
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int index = Arrays.binarySearch(intervals, newInterval, Comparator.comparingInt(o -> o[0]));
+        return null;
+    }
+
+    private void binarySearch() {
+
     }
 
     public static void main(String[] args) {
-        int[][] intervals = new int[][]{
-                new int[]{1, 3},
-                new int[]{8, 10},
-                new int[]{2, 6},
-                new int[]{15, 18}
+        int[][] data = new int[][]{
+                new int[]{1, 2},
+                new int[]{3, 5},
         };
-        new Solution().merge(intervals);
+        int[] newInterval = new int[]{2, 3};
+        new Solution().insert(data, newInterval);
     }
 }
