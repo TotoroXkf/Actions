df9aa753d5ddcd7d1df3f23e27e9cf5dd875a580
xiakaifa <xkf123456789@gmail.com>
2020-01-16 10:11:31 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,49 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return new ArrayList<>();
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][0];
         }
-        int left = 0;
-        int top = 0;
-        int right = matrix[0].length - 1;
-        int bottom = matrix.length - 1;
-        List<Integer> result = new ArrayList<>();
-        while (left <= right && top <= bottom) {
-            if (left == right && top == bottom) {
-                result.add(matrix[top][left]);
-                break;
-            } else if (left == right) {
-                for (int i = top; i <= bottom; i++) {
-                    result.add(matrix[i][left]);
-                }
-                break;
-            } else if (top == bottom) {
-                for (int i = left; i <= right; i++) {
-                    result.add(matrix[top][i]);
-                }
-                break;
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+        int[][] result = new int[intervals.length][2];
+        int currentIndex = 0;
+        int[] currentInterval = new int[2];
+        currentInterval[0] = intervals[0][0];
+        currentInterval[1] = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            int value1 = intervals[i][0];
+            int value2 = intervals[i][1];
+            if (value1 <= currentInterval[1]) {
+                currentInterval[1] = Math.max(currentInterval[1], value2);
+            } else {
+                result[currentIndex++] = currentInterval;
+                currentInterval = new int[2];
+                currentInterval[0] = intervals[i][0];
+                currentInterval[1] = intervals[i][1];
             }
-
-            for (int i = left; i < right; i++) {
-                result.add(matrix[top][i]);
-            }
-            for (int i = top; i < bottom; i++) {
-                result.add(matrix[i][right]);
-            }
-            for (int i = right; i > left; i--) {
-                result.add(matrix[bottom][i]);
-            }
-            for (int i = bottom; i > top; i--) {
-                result.add(matrix[i][left]);
-            }
-            left++;
-            right--;
-            top++;
-            bottom--;
         }
+        result[currentIndex++] = currentInterval;
+        result = Arrays.copyOf(result, currentIndex);
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = new int[][]{
+                new int[]{1, 3},
+                new int[]{8, 10},
+                new int[]{2, 6},
+                new int[]{15, 18}
+        };
+        new Solution().merge(intervals);
     }
 }
