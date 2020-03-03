ada5ddae553d3ced4cf287b9964ec45422cf139e
xiakaifa <xkf123456789@gmail.com>
2020-03-03 09:38:01 +0800

    no message

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -2,37 +2,71 @@ import java.util.LinkedList;
 
 class Solution {
     /**
-     * 比较好的一道题
-     * 这个题的关键就在于，找到当前位置左边第一个比它小的值和右边第一个比它小的值的位置
-     * 然后就可以计算出来当前位置的围成面积
-     * 整体遍历之后就可以得出最大值
+     * 柱状图中最大的矩形 问题的延伸问题
+     * 对于一个矩形，假设只有一行
+     * [1,0,1,0,0]
+     * 可以直接把他看成一个移位数组，，然后得到答案
+     * 两行的话
+     * [1,0,1,0,0]
+     * [0,1,1,0,1]
+     * 这个时候从底下网上看，就可以得到
+     * [0,1,2,0,1]
+     * 实际上，当前行如果为0，就不要这个位置了。如果为1，就选择继承上方的值即可
+     * 这样从上到下逐步的将值叠加，遇到0丢弃，遇到1继承，每一行都做相同的处理，再传递给函数求值
+     * 最后记录最大值即可
      */
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int[] line = new int[matrix[0].length];
+        // 第一行处理
+        for (int i = 0; i < matrix[0].length; i++) {
+            line[i] = matrix[0][i] - '0';
+        }
+        int max = largestRectangleArea(line);
+        for (int i = 1; i < matrix.length; i++) {
+            // 前一行的值
+            char[] preLine = matrix[i - 1];
+            // 当前行的值
+            char[] currentLine = matrix[i];
+            for (int j = 0; j < currentLine.length; j++) {
+                // 遇到0直接丢弃
+                if (currentLine[j] - '0' == 0) {
+                    line[j] = 0;
+                    continue;
+                }
+                // 遇到1，继承之前位置的值
+                int value = 1 + (preLine[j] - '0');
+                line[j] = value;
+                currentLine[j] = (char) (value + '0');
+            }
+            // 比较新结果
+            max = Math.max(largestRectangleArea(line), max);
+        }
+        return max;
+    }
+
     public int largestRectangleArea(int[] heights) {
         LinkedList<Integer> stack = new LinkedList<>();
-        // 记录第一个最小值的位置
         int[] leftFirstMinIndex = new int[heights.length];
         int[] rightFirstMinIndex = new int[heights.length];
-        // 默认左右两边无最小值的位置，方便计算
         for (int i = 0; i < heights.length; i++) {
             leftFirstMinIndex[i] = -1;
             rightFirstMinIndex[i] = heights.length;
         }
         for (int i = 0; i < heights.length; i++) {
-            // 使用栈结构维持升序，这样栈顶就代表了左边遇到的可用最大值。和当前值逐一比较，出栈，就可以快速的找到左边的第一个小于当前值的坐标
             while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
                 int index = stack.pop();
-                // 弹出一个位置下标，比当前元素大，说明当前位置右边第一个最小值就是当前值
                 rightFirstMinIndex[index] = i;
             }
             if (!stack.isEmpty()) {
-                // 找到左边第一个小于的值，记录下标
                 leftFirstMinIndex[i] = stack.peek();
             }
             stack.push(i);
         }
         int max = 0;
         for (int i = 0; i < heights.length; i++) {
-            // 计算面积
             int area = (rightFirstMinIndex[i] - leftFirstMinIndex[i] - 1) * heights[i];
             max = Math.max(area, max);
         }
