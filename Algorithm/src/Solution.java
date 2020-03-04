071dd75adc8d172a32e01b62c334117813f2b3ce
xiakaifa <xkf123456789@gmail.com>
2020-03-04 23:11:59 +0800

    no message

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,5 +1,18 @@
 class Solution {
-    public boolean CheckPermutation(String s1, String s2) {
-        
+    public String replaceSpaces(String s, int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String space = "%20";
+        int i = 0;
+        int len = 0;
+        while (i < s.length() && len < length) {
+            if (s.charAt(i) == ' ') {
+                stringBuilder.append(space);
+            } else {
+                stringBuilder.append(s.charAt(i));
+            }
+            len++;
+            i++;
+        }
+        return stringBuilder.toString();
     }
 }
