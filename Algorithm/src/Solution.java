482b2265d6aeefcc9058892ca937ed0d75fcb6df
xiakaifa <xkf123456789@gmail.com>
2020-03-17 08:14:58 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,19 +1,21 @@
 class Solution {
     /**
-     * 判断二叉搜索树是面试经常遇到的问题
-     * 其实就是设置左右边界，看当前的节点是不是在这个边界里面即可
+     * 寻找儿叉搜索树的下一个节点，也是面试经常会问到的问题
+     * 一般来说，二叉树无外乎就是递归左子树和右子树
+     * 假设当前的节点比目标节点值大
+     * 说明目标节点值在当前节点的左子树上。那么最终的结果就一定出在左子树上，或者是当前节点
+     * 那就可以递归左子树，若果有结果，那么一定是比当前节点更加靠近目标节点值的。返回即可。如果无结果，当前节点顺理成章的成为了后继节点
+     * 若果是当前节点比目标节点小，那就说明目标节点在右子树上
+     * 无论如何，后继节点都喝当前节点没有关系，直接递归右子树即可
      */
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-
-    private boolean isValidBST(TreeNode root, long atLeast, long atMost) {
-        if (root == null) {
-            return true;
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
         }
-        if (root.val <= atLeast || root.val >= atMost) {
-            return false;
+        if (root.val > p.val) {
+            TreeNode leftResult = inorderSuccessor(root.left, p);
+            return leftResult != null ? leftResult : root;
         }
-        return isValidBST(root.left, atLeast, root.val) && isValidBST(root.right, root.val, atMost);
+        return inorderSuccessor(root.right, p);
     }
 }
---
+++ DaggerTest/app/src/main/java/com/xkf/daggertest/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.xkf.daggertest
+
+/**
+ * author : xiakaifa
+ * 2020/3/16
+ */
+class UserRepository {
+}
