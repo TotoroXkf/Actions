a38ee87905a22150b61662a8f6ec7c02cb48a509
xiakaifa <xkf123456789@gmail.com>
2020-03-22 19:37:08 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,18 +1,3 @@
 class Solution {
-    public String printBin(double num) {
-        if (num >= 1 || num < 0) {
-            return "ERROR";
-        }
-        StringBuilder sb = new StringBuilder("0.");
-        for (int i = 1; i < 31 && num > 0; i++) {
-            double value = Math.pow(0.5, i);
-            if (num >= value) {
-                num -= value;
-                sb.append('1');
-            } else {
-                sb.append('0');
-            }
-        }
-        return num == 0 ? sb.toString() : "ERROR";
-    }
+
 }
