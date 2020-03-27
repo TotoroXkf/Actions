0c65cb1408e0cb0a5ce2ee0d18d81a34a2c67da3
xiakaifa <xkf123456789@gmail.com>
2020-03-27 12:23:23 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,20 +1,16 @@
 class Solution {
-    /**
-     * 思路的话就是分别取出奇数位和偶数位，移动后做或运算。
-     * 题目规定 num 是int范围的数
-     * 0x55555555 = 0101_0101_0101_0101_0101_0101_0101_0101
-     * 0xaaaaaaaa = 1010_1010_1010_1010_1010_1010_1010_1010
-     * 用这两个数做与运算，就可以把奇数位和偶数位取出来，
-     * 然后位左移奇数位，右移偶数位，
-     * 再把 奇数位和偶数位做或运算。
-     */
-    public int exchangeBits(int num) {
-        //奇数
-        int value1 = num & 0x55555555;
-        //偶数
-        int value2 = num & 0xaaaaaaaa;
-        value1 = value1 << 1;
-        value2 = value2 >> 1;
-        return value1 | value2;
+    public int waysToStep(int n) {
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            if (i == 2) {
+                dp[i] = dp[1] + 1;
+            } else if (i == 3) {
+                dp[i] = dp[i - 1] + dp[i - 2] + 1;
+            } else {
+                dp[i] = ((dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000007);
+            }
+        }
+        return (int) dp[n];
     }
 }
