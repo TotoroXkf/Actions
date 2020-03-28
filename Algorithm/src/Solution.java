c362f233a9b789fabf2b8ca1804f3f9395f2e9fb
xiakaifa <xkf123456789@gmail.com>
2020-03-28 09:15:31 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,16 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int waysToStep(int n) {
-        long[] dp = new long[n + 1];
-        dp[1] = 1;
-        for (int i = 2; i < n + 1; i++) {
-            if (i == 2) {
-                dp[i] = dp[1] + 1;
-            } else if (i == 3) {
-                dp[i] = dp[i - 1] + dp[i - 2] + 1;
-            } else {
-                dp[i] = ((dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000007);
-            }
+    public List<List<Integer>> pathWithObstacles(int[][] obstacleGrid) {
+        List<List<Integer>> result = new ArrayList<>();
+        pathWithObstacles(obstacleGrid, result, 0, 0);
+        return result;
+    }
+
+    public boolean pathWithObstacles(int[][] obstacleGrid, List<List<Integer>> answer, int row, int column) {
+        if (column >= obstacleGrid[0].length || row >= obstacleGrid.length) {
+            return false;
         }
-        return (int) dp[n];
+        if (obstacleGrid[row][column] == 1) {
+            return false;
+        }
+        List<Integer> point = new ArrayList<>();
+        point.add(row);
+        point.add(column);
+        answer.add(point);
+        if (row == obstacleGrid.length - 1 && column == obstacleGrid[0].length - 1) {
+            return true;
+        }
+        boolean rightResult = pathWithObstacles(obstacleGrid, answer, row, column + 1);
+        if (rightResult) {
+            return true;
+        }
+        boolean bottomResult = pathWithObstacles(obstacleGrid, answer, row + 1, column);
+        if (bottomResult) {
+            return true;
+        }
+        obstacleGrid[row][column] = 1;
+        answer.remove(answer.size() - 1);
+        return false;
     }
 }
