643ddf87ae40846919ea98e100cb254ca7aa1c99
xiakaifa <xkf123456789@gmail.com>
2020-03-29 17:51:46 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,28 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    /**
-     * 举个例子
-     * 10 * 4 = (2+2+2+2+2) * 4 = (2^3+2^1) *4  = 4<<3 + 4<<1
-     * 所以这个题的关键在于怎么找到这个3和1
-     * 10的二进制正好代表了这个
-     * 1010
-     * 对应的位减去1就是2的阶乘数。逐步取出来，左移，相加即可
-     */
-    public int multiply(int a, int b) {
-        if (a == 0 || b == 0) {
-            return 0;
-        }
-        int mask = 1;
-        int pow = 0;
-        int result = 0;
-        while (mask != 0) {
-            // 不断左移取出1
-            if ((a & mask) != 0) {
-                // 加上结果
-                result += (b << pow);
-            }
-            mask <<= 1;
-            pow++;
-        }
+    public String[] permutation(String s) {
+        boolean[] marked = new boolean[s.length()];
+        List<String> list = new ArrayList<>();
+        permutation(s, marked, list, new StringBuilder());
+        String[] result = new String[list.size()];
+        list.toArray(result);
         return result;
+    }
+
+    public void permutation(String s, boolean[] marked, List<String> result, StringBuilder stringBuilder) {
+        if (stringBuilder.length() == s.length()) {
+            result.add(stringBuilder.toString());
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (marked[i]) {
+                continue;
+            }
+            stringBuilder.append(s.charAt(i));
+            marked[i] = true;
+            permutation(s, marked, result, stringBuilder);
+            marked[i] = false;
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
     }
 }
