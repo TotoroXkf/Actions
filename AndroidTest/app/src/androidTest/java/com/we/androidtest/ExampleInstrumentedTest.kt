461e338d3911d6ca1945a98fdb3227416e74f9f5
xiakaifa <xkf123456789@gmail.com>
2020-04-26 17:31:57 +0800

    no message

--- AndroidTest/app/src/androidTest/java/com/we/androidtest/ExampleInstrumentedTest.kt
+++ AndroidTest/app/src/androidTest/java/com/we/androidtest/ExampleInstrumentedTest.kt
@@ -1,22 +1,38 @@
 package com.we.androidtest
 
-import android.content.Context
-import android.content.pm.ApplicationInfo
-import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
-    val context: Context = ApplicationProvider.getApplicationContext()
+    @Rule
+    @JvmField
+    var activityRule: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
 
     @Test
     fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.we.androidtest", appContext.packageName)
+        Espresso.onView(ViewMatchers.withId(R.id.editText))
+            .perform(ViewActions.typeText("This is a test"), ViewActions.closeSoftKeyboard())
+        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
+
+        Intents.intended(
+            allOf(
+                hasComponent(hasShortClassName(".Main2Activity")),
+                toPackage("com.we.androidtest"),
+                hasExtra(MainActivity.EXTRA_MESSAGE, "This is a test")
+            )
+        )
     }
 }
--- AndroidTest/app/src/main/java/com/we/androidtest/MainActivity.kt
+++ AndroidTest/app/src/main/java/com/we/androidtest/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.we.androidtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_MESSAGE = "message"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            intent.putExtra(EXTRA_MESSAGE, "This is a test")
+            startActivity(intent)
+        }
     }
 }
