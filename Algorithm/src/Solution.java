3b6cdd0746344704822fc846bb6b743526ba586c
xiakaifa <xkf123456789@gmail.com>
2020-05-06 08:52:21 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,9 +1,31 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-        int sum = (nums.length * (nums.length + 1)) / 2;
-        for (int num : nums) {
-            sum -= num;
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int temp = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            // 遇到相同的+1，不相同的减去1
+            if (temp == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            // 如果count减少到了0，说明这个元素在i这个位置，值为temp的元素全部被抵消了，重新选择当前元素开始下一轮
+            if (count == 0) {
+                temp = nums[i];
+                count = 1;
+            }
         }
-        return sum;
+        // 最后确定下来的元素所代表的含义就是，数组中每两个不相同的数字相互抵消，所剩下的数字就是temp
+        // 这个temp是唯一可能成为主要元素的结果，这里再做一次验证
+        count = 0;
+        for (int num : nums) {
+            if (num == temp) {
+                count++;
+            }
+            if (count > nums.length / 2) {
+                return num;
+            }
+        }
+        return -1;
     }
 }
--- Algorithm/src/SolutionTest.java
+++ Algorithm/src/SolutionTest.java
@@ -6,28 +6,20 @@ public class SolutionTest {
 
     @Test
     public void test() {
-        int[] square1 = new int[]{-1, -1, 2};
-        int[] square2 = new int[]{-1, 10, 2};
-        double[] result = solution.cutSquares(square1, square2);
-        assert result[0] == 0;
-        assert result[1] == -1;
-        assert result[2] == 0;
-        assert result[3] == 12;
+        int[] array = new int[]{1, 2, 5, 9, 5, 9, 5, 5, 5};
+        int result = solution.majorityElement(array);
+        assert result == 5;
 
-        square1 = new int[]{-1, -1, 2};
-        square2 = new int[]{0, -1, 2};
-        result = solution.cutSquares(square1, square2);
-        assert result[0] == -1;
-        assert result[1] == 0;
-        assert result[2] == 2;
-        assert result[3] == 0;
+        array = new int[]{3, 2};
+        result = solution.majorityElement(array);
+        assert result == -1;
 
-        square1 = new int[]{-1, -1, 3};
-        square2 = new int[]{-1, 10, 2};
-        result = solution.cutSquares(square1, square2);
-        assert result[0] == -0.04762;
-        assert result[1] == 12.00000;
-        assert result[2] == 0.57143;
-        assert result[3] == -1.00000;
+        array = new int[]{2, 2, 1, 1, 1, 2, 2};
+        result = solution.majorityElement(array);
+        assert result == 2;
+
+        array = new int[]{3, 2, 3};
+        result = solution.majorityElement(array);
+        assert result == 3;
     }
 } 
