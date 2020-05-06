dee0ef2eb3d2302fa434388708235628cdb143f0
xiakaifa <xkf123456789@gmail.com>
2020-05-06 10:24:47 +0800

    Coding

--- AndroidTest/app/src/androidTest/java/com/we/androidtest/ExampleInstrumentedTest.kt
+++ AndroidTest/app/src/androidTest/java/com/we/androidtest/ExampleInstrumentedTest.kt
@@ -1,22 +1,14 @@
 package com.we.androidtest
 
-import android.app.Activity
-import android.app.Instrumentation
 import android.content.Intent
-import android.net.Uri
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.ComponentNameMatchers
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.web.assertion.WebViewAssertions
+import androidx.test.espresso.web.sugar.Web
+import androidx.test.espresso.web.webdriver.DriverAtoms
+import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.GrantPermissionRule
+import androidx.test.rule.ActivityTestRule
 import org.hamcrest.Matchers
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,50 +16,58 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ExampleInstrumentedTest {
-    private val VALID_PHONE_NUMBER = "123-345-6789"
-    private val INTENT_DATA_PHONE_NUMBER: Uri = Uri.parse("tel:$VALID_PHONE_NUMBER")
-
-
-    @get:Rule
-    val grantPermissionRule = GrantPermissionRule.grant("android.permission.CALL_PHONE")
+    private val MACCHIATO = "Macchiato"
+    private val DOPPIO = "Doppio"
 
     @get:Rule
-    val activityRule = IntentsTestRule(MainActivity::class.java)
-
-    @Before
-    fun stubAllExternalIntents() {
-        Intents.intending(Matchers.not(IntentMatchers.isInternal()))
-            .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
-    }
-
-    @Test
-    fun typeNumberValidInputInitiatesCall() {
-        Espresso.onView(ViewMatchers.withId(R.id.edit_text_caller_number))
-            .perform(
-                ViewActions.typeText(VALID_PHONE_NUMBER),
-                ViewActions.closeSoftKeyboard()
-            )
-        Espresso.onView(ViewMatchers.withId(R.id.button_call_number)).perform(ViewActions.click())
-
-        Intents.intended(
-            Matchers.allOf(
-                IntentMatchers.hasAction(Intent.ACTION_CALL),
-                IntentMatchers.hasData(INTENT_DATA_PHONE_NUMBER)
-            )
-        )
-    }
+    val activityRule =
+        object : ActivityTestRule<MainActivity>(MainActivity::class.java, false, false) {
+            override fun afterActivityLaunched() {
+                Web.onWebView().forceJavascriptEnabled()
+            }
+        }
 
     @Test
-    fun pickContactButton_click_SelectsPhoneNumber() {
-        Intents.intending(IntentMatchers.hasComponent(ComponentNameMatchers.hasShortClassName(".Main2Activity")))
-            .respondWith(
-                Instrumentation.ActivityResult(
-                    Activity.RESULT_OK,
-                    Main2Activity.createResultData(VALID_PHONE_NUMBER)
+    fun typeTextInInputClickButtonSubmitsForm() {
+        activityRule.launchActivity(withWebFormIntent())
+
+        Web.onWebView()
+            .withElement(DriverAtoms.findElement(Locator.ID, "text_input"))
+            .perform(DriverAtoms.clearElement())
+            .perform(DriverAtoms.webKeys(MACCHIATO))
+            .withElement(DriverAtoms.findElement(Locator.ID, "submitBtn"))
+            .perform(DriverAtoms.webClick())
+            .withElement(DriverAtoms.findElement(Locator.ID, "response"))
+            .check(
+                WebViewAssertions.webMatches(
+                    DriverAtoms.getText(),
+                    Matchers.containsString(MACCHIATO)
                 )
             )
-        Espresso.onView(ViewMatchers.withId(R.id.button_pick_contact)).perform(ViewActions.click());
-        Espresso.onView(ViewMatchers.withId(R.id.edit_text_caller_number))
-            .check(ViewAssertions.matches(ViewMatchers.withText(VALID_PHONE_NUMBER)));
     }
+
+    @Test
+    fun typeTextInInputClickButtonChangesText() {
+        activityRule.launchActivity(withWebFormIntent())
+
+        Web.onWebView()
+            .withElement(DriverAtoms.findElement(Locator.ID, "text_input"))
+            .perform(DriverAtoms.clearElement())
+            .perform(DriverAtoms.webKeys(DOPPIO))
+            .withElement(DriverAtoms.findElement(Locator.ID, "changeTextBtn"))
+            .perform(DriverAtoms.webClick())
+            .withElement(DriverAtoms.findElement(Locator.ID, "message"))
+            .check(
+                WebViewAssertions.webMatches(
+                    DriverAtoms.getText(),
+                    Matchers.containsString(DOPPIO)
+                )
+            )
+    }
+}
+
+fun withWebFormIntent(): Intent {
+    val basicFormIntent = Intent()
+    basicFormIntent.putExtra(KEY_URL_TO_LOAD, WEB_FORM_URL)
+    return basicFormIntent
 }
--- AndroidTest/app/src/main/java/com/we/androidtest/MainActivity.kt
+++ AndroidTest/app/src/main/java/com/we/androidtest/MainActivity.kt
@@ -1,55 +1,39 @@
 package com.we.androidtest
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.text.TextUtils
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
+val KEY_URL_TO_LOAD = "KEY_URL_TO_LOAD"
 
-class MainActivity : AppCompatActivity() {
-    private val REQUEST_CODE_PICK = 16
+@VisibleForTesting
+val WEB_FORM_URL = "file:///android_asset/web_form.html"
 
+open class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun onCall(view: View) {
-        val hasCallPhonePermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.CALL_PHONE
-        ) == PackageManager.PERMISSION_GRANTED
-        if (hasCallPhonePermission)
-            startActivity(createCallIntentFromNumber())
-        else
-            Toast.makeText(this, "没有权限", Toast.LENGTH_SHORT).show()
-    }
-
-    fun onPickContact(view: View) {
-        val pickContactIntent = Intent(this, Main2Activity::class.java)
-        startActivityForResult(pickContactIntent, REQUEST_CODE_PICK)
-    }
-
-    private fun createCallIntentFromNumber(): Intent? {
-        val intentToCall = Intent(Intent.ACTION_CALL)
-        val number: String = edit_text_caller_number.getText().toString()
-        intentToCall.data = Uri.parse("tel:$number")
-        return intentToCall
-    }
-
-    @SuppressLint("MissingSuperCall")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE_PICK) {
-            if (resultCode == RESULT_OK) {
-                edit_text_caller_number.setText(data?.extras?.getString(Main2Activity.KEY_PHONE_NUMBER))
+        web_view.settings.javaScriptEnabled = true
+        web_view.loadUrl(urlFromIntent(intent))
+        web_view.requestFocus()
+        web_view.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return false
             }
         }
+    }
+
+    private fun urlFromIntent(intent: Intent): String? {
+        val url = intent.getStringExtra(KEY_URL_TO_LOAD)
+        return if (!TextUtils.isEmpty(url)) url else WEB_FORM_URL
     }
 }
