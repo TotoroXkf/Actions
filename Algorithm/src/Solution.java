bef4eb57452744e9778027f9f3150a149146881c
xiakaifa <xkf123456789@gmail.com>
2020-05-11 08:55:51 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,12 +1,23 @@
+import java.util.HashMap;
+
 class Solution {
-    public int add(int a, int b) {
-        int sum = a;
-        while (b != 0) {
-            sum = a ^ b;
-            int up = (a & b) << 1;
-            a = sum;
-            b = up;
+    public int[] masterMind(String solution, String guess) {
+        int[] result = new int[2];
+        HashMap<Character, Integer> solutionHasMap = new HashMap<>();
+        HashMap<Character, Integer> guessHashMap = new HashMap<>();
+        for (int i = 0; i < solution.length(); i++) {
+            if (solution.charAt(i) == guess.charAt(i)) {
+                result[0]++;
+            } else {
+                solutionHasMap.put(solution.charAt(i), solutionHasMap.getOrDefault(solution.charAt(i), 0) + 1);
+                guessHashMap.put(guess.charAt(i), guessHashMap.getOrDefault(guess.charAt(i), 0) + 1);
+            }
         }
-        return sum;
+        for (Character character : guessHashMap.keySet()) {
+            if (solutionHasMap.containsKey(character)) {
+                result[1] += Math.min(solutionHasMap.get(character), guessHashMap.get(character));
+            }
+        }
+        return result;
     }
 }
