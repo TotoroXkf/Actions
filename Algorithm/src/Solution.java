81116a9a9e2b31459cd7260904d6666af8092e27
xiakaifa <xkf123456789@gmail.com>
2020-05-12 10:28:08 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 
 class Solution {
     public int[] bestLine(int[][] points) {
-        LinkedHashMap<Double, ArrayList<Integer>> hashMap = new LinkedHashMap<>();
+        HashMap<Double, ArrayList<Integer>> hashMap = new HashMap<>();
         int maxLen = 0;
         int[] result = new int[2];
         for (int i = 0; i < points.length; i++) {
@@ -12,13 +12,16 @@ class Solution {
             for (int j = i + 1; j < points.length; j++) {
                 int x2 = points[j][0];
                 int y2 = points[j][1];
+                // 确定通过起始点的直线，斜率相同则一定在同一条直线上面
                 double k = getK(x1, y1, x2, y2);
                 ArrayList<Integer> arrayList = hashMap.getOrDefault(k, new ArrayList<>());
+                // 记录在同一条直线上面的点
                 if (arrayList.isEmpty()) {
                     arrayList.add(i);
                     hashMap.put(k, arrayList);
                 }
                 arrayList.add(j);
+                // 比较最大值
                 if (arrayList.size() > maxLen) {
                     maxLen = arrayList.size();
                     result[0] = arrayList.get(0);
@@ -31,9 +34,11 @@ class Solution {
     }
 
     private double getK(int x1, int y1, int x2, int y2) {
+        // 在java中 0d和-0d在 hashmap 中的 比较会不一样，所以统一返回0d
         if (y1 - y2 == 0) {
             return 0d;
         }
+        // 在java中 无穷分为正无穷和负无穷， hashmap 中的 比较会不一样，所以统一返回正无穷
         if (x1 == x2) {
             return Double.POSITIVE_INFINITY;
         }
