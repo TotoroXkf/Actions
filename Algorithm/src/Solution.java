b05d6df1f0d922b07a08dbcec80a65638d95099d
xiakaifa <xkf123456789@gmail.com>
2020-05-20 18:11:25 +0800

    Coding

--- Algorithm/src/Solution.java
+++ Algorithm/src/Solution.java
@@ -1,85 +1,84 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 class Solution {
     public List<String> printKMoves(int k) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.put("0,0", 1);
+        // 使用set记录黑块的位置
+        HashSet<Position> hashSet = new HashSet<>();
+        int currentX = 0, currentY = 0, minX = 0, maxX = 0, minY = 0, maxY = 0;
+        int[][] updateDirection = new int[][]{new int[]{0, -1}, new int[]{1, 0}, new int[]{0, 1}, new int[]{-1, 0}};
         int direction = 1;
-        int minX = 0, minY = 0, maxX = 0, maxY = 0;
-        int currentX = 0, currentY = 0;
+        // 模拟移动
         for (int i = 0; i < k; i++) {
-            String key = currentX + "," + currentY;
-            int value = hashMap.getOrDefault(key, 1);
-            hashMap.put(key, -value);
-            if (value == 1) {
+            Position position = new Position(currentX, currentY);
+            // 添加成功说明当前是白色的块
+            if (hashSet.add(position)) {
                 direction++;
+                direction %= 4;
             } else {
+                // 添加失败说明是黑色的块
                 direction--;
+                if (direction == -1) {
+                    direction = 3;
+                }
+                hashSet.remove(position);
             }
-            direction %= 4;
-            switch (direction) {
-                case 0:
-                    currentY--;
-                    break;
-                case 1:
-                    currentX++;
-                    break;
-                case 2:
-                    currentY++;
-                    break;
-                case 3:
-                    currentX--;
-                    break;
-            }
+            // 移动
+            currentX += updateDirection[direction][0];
+            currentY += updateDirection[direction][1];
+            // 确定边界
             minX = Math.min(minX, currentX);
-            minY = Math.min(minY, currentY);
             maxX = Math.max(maxX, currentX);
+            minY = Math.min(minY, currentY);
             maxY = Math.max(maxY, currentY);
         }
-        boolean[][] map = new boolean[maxY - minY + 1][maxX - minX + 1];
-        for (String key : hashMap.keySet()) {
-            int index = key.indexOf(',');
-            int x = Integer.parseInt(key.substring(0, index)) - minX;
-            int y = Integer.parseInt(key.substring(index + 1)) - minY;
-            int value = hashMap.get(key);
-            if (value == -1) {
-                map[y][x] = true;
-            }
+        char[][] map = new char[maxY - minY + 1][maxX - minX + 1];
+        // 填充白色
+        for (char[] chars : map) {
+            Arrays.fill(chars, '_');
         }
+        // 填充黑色
+        for (Position position : hashSet) {
+            int x = position.x - minX;
+            int y = position.y - minY;
+            map[y][x] = 'X';
+        }
+        // 设置蚂蚁的位置
+        char[] directionChar = new char[]{'U', 'R', 'D', 'L'};
         currentX -= minX;
         currentY -= minY;
+        map[currentY][currentX] = directionChar[direction];
         List<String> result = new ArrayList<>();
-        for (int i = 0; i < map.length; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int j = 0; j < map[0].length; j++) {
-                if (i == currentY && j == currentX) {
-                    switch (direction) {
-                        case 0:
-                            stringBuilder.append("U");
-                            break;
-                        case 1:
-                            stringBuilder.append("R");
-                            break;
-                        case 2:
-                            stringBuilder.append("D");
-                            break;
-                        case -1:
-                        case 3:
-                            stringBuilder.append("L");
-                            break;
-                    }
-                    continue;
-                }
-                if (map[i][j]) {
-                    stringBuilder.append("X");
-                } else {
-                    stringBuilder.append("_");
-                }
-            }
-            result.add(stringBuilder.toString());
+        for (char[] chars : map) {
+            result.add(new String(chars));
         }
         return result;
     }
+
+    private static class Position {
+        int x, y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (!(obj instanceof Position)) return false;
+            Position o = (Position) obj;
+            return x == o.x && y == o.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+    }
+
 }
