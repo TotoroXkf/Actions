f0ab57f0dcdaa310e0108aad1169372ab4bd897d
xiakaifa <xkf123456789@gmail.com>
2020-05-26 10:45:36 +0800

    no message

--- ppjoke/libannotation/src/main/java/com/xkf/libannotation/FragmentDestination.kt
+++
@@ -1,8 +0,0 @@
-package com.xkf.libannotation
-
-@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
-annotation class FragmentDestination(
-    val pageUrl: String,
-    val needLogin: Boolean = false,
-    val asStarter: Boolean = false
-)
--- ppjoke/libnetwork/src/main/java/com/xkf/libnetwork/ApiResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.xkf.libnetwork
-
-class ApiResponse<T>(
-    var success: Boolean = false,
-    var status: Int = 0,
-    var message: String = "",
-    var body: T? = null
-)
