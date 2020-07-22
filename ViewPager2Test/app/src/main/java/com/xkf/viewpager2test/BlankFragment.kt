7915603ee1ab95672d2b8a62e8fd97fa9370d06b
xiakaifa <xkf123456789@gmail.com>
2020-07-22 15:36:19 +0800

    Coding

---
+++ ViewPager2Test/app/src/main/java/com/xkf/viewpager2test/BlankFragment.kt
@@ -0,0 +1,26 @@
+package com.xkf.viewpager2test
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class BlankFragment(val position: Int) : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textView = view.findViewById<TextView>(R.id.textView)
+        textView.text = "第${position}页"
+    }
+}
---
+++ ViewPager2Test/app/src/main/java/com/xkf/viewpager2test/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.xkf.viewpager2test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = ViewPagerAdapter(this)
+
+        viewPager.apply {
+            offscreenPageLimit = 1
+            val recyclerView = getChildAt(0) as RecyclerView
+            recyclerView.apply {
+                val padding = 60
+                setPadding(padding, 0, padding, 0)
+                clipToPadding = false
+            }
+        }
+
+        val compositePageTransformer = CompositePageTransformer()
+        compositePageTransformer.addTransformer(MarginPageTransformer(20))
+        compositePageTransformer.addTransformer(ScaleInTransformer())
+        viewPager.setPageTransformer(compositePageTransformer)
+    }
+
+    class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
+        override fun getItemCount(): Int = 20
+
+        override fun createFragment(position: Int): Fragment {
+            return BlankFragment(position)
+        }
+    }
+}
