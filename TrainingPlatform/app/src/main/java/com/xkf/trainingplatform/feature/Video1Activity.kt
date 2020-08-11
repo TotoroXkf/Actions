1c8c3cd883809cec979ea2393c8ffc2e4fd03f3e
xiakaifa <xkf123456789@gmail.com>
2020-08-11 15:53:36 +0800

    Coding

---
+++ TrainingPlatform/app/src/main/java/com/xkf/trainingplatform/feature/Video1Activity.kt
@@ -0,0 +1,12 @@
+package com.xkf.trainingplatform.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.xkf.trainingplatform.R
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
