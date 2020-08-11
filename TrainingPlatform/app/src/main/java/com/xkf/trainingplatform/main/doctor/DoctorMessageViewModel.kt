39a3585550eb2e55ea1bb73ed89c69d66dba8fd9
xiakaifa <xkf123456789@gmail.com>
2020-08-11 11:01:51 +0800

    Coding

---
+++ TrainingPlatform/app/src/main/java/com/xkf/trainingplatform/main/doctor/DoctorMessageViewModel.kt
@@ -0,0 +1,9 @@
+package com.xkf.trainingplatform.main.doctor
+
+/**
+ * @author xiakaifa
+ * 功能描述:
+ * 时 间： 2020/8/11 10:28
+ */
+class DoctorMessageViewModel {
+}
---
+++ TrainingPlatform/app/src/main/java/com/xkf/trainingplatform/model/ChatMessage.kt
@@ -0,0 +1,9 @@
+package com.xkf.trainingplatform.model
+
+/**
+ * @author xiakaifa
+ * 功能描述:
+ * 时 间： 2020/8/11 10:28
+ */
+class ChatMessage {
+}
