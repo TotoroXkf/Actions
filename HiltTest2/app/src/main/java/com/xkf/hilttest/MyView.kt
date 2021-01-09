e64baa102d3fbf90f1bc2ec554134673fee26eaf
xiakaifa <xkf123456789@gmail.com>
2021-01-10 07:39:54 +0800

    Coding

---
+++ HiltTest2/app/src/main/java/com/xkf/hilttest/MyView.kt
@@ -0,0 +1,9 @@
+package com.xkf.hilttest
+
+/**
+ * @author xiakaifa
+ * 功能描述:
+ * 时 间： 2021/1/5 19:56
+ */
+class MyView {
+}
---
+++ TestApplication/app/src/main/java/com/xkf/testapplication/ViewModel.kt
@@ -0,0 +1,9 @@
+package com.xkf.testapplication
+
+/**
+ * @author xiakaifa
+ * 功能描述:
+ * 时 间： 2020/10/21 11:24
+ */
+class ViewModel {
+}
