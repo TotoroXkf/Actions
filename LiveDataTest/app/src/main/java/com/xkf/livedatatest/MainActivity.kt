e68bb783e3aeab10d46db86d5d6759360b640877
xiakaifa <xkf123456789@gmail.com>
2021-01-10 14:44:06 +0800

    Coding

--- LiveDataTest/app/src/main/java/com/xkf/livedatatest/MainActivity.kt
+++ LiveDataTest/app/src/main/java/com/xkf/livedatatest/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.xkf.livedatatest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.observe
 
 class MainActivity : AppCompatActivity() {
+    private val liveData = MutableLiveData<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myLiveData = MyLiveData()
+        myLiveData.observe(this) {
+
+        }
+        myLiveData.observe(this) {
+
+        }
+    }
+
+    class MyLiveData : LiveData<String>() {
+        override fun onActive() {
+            super.onActive()
+
+            Log.e("大龙猫", "onActive: ")
+        }
+
+        override fun onInactive() {
+            super.onInactive()
+
+            Log.e("大龙猫", "onInactive: ")
+        }
     }
 }
